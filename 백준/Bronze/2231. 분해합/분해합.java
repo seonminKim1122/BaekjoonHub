6067c1d08f7953bd7b1d6099bceafa9d5f6fd16a
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-03-15 11:15:18 +0900

    [Bronze II] Title: 분해합, Time: 204 ms, Memory: 17672 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -2,19 +2,18 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int len = String.valueOf(N).length();
-        int min = N - 9*(len-1) - N/(int)Math.pow(10, len-1);
+        String s = sc.nextLine();
+        int len = s.length();
+        int N = Integer.parseInt(s);
+        int min = N - 9*(len-1) - (N/(int) Math.pow(10, len-1) - 1);
 
         int result = 0;
         for (int i = min; i < N; i++) {
-            String number = String.valueOf(i);
-
+            String num = String.valueOf(i);
             int decompositionSum = i;
-            for (String n : number.split("")) {
+            for (String n : num.split("")) {
                 decompositionSum += Integer.parseInt(n);
             }
-
             if (decompositionSum == N) {
                 result = i;
                 break;

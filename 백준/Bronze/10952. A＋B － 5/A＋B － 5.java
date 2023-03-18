8990bca03dd89c91a8f92aee0190cf2463f508f0
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-03-18 18:45:47 +0900

    [Bronze V] Title: A+B - 5, Time: 140 ms, Memory: 15884 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            if (A+B == 0) break;
+            bw.write((A+B) + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}

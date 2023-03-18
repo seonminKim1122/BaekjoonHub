2366d3b503cb6058f974e1904c1b3614e8db6ee2
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-03-18 17:25:35 +0900

    [Bronze V] Title: 사분면 고르기, Time: 208 ms, Memory: 17708 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -4,12 +4,11 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
         int y = sc.nextInt();
-        int result;
+
         if (x > 0) {
-            result = y > 0 ? 1 : 4;
+            System.out.println(y > 0 ? 1 : 4);
         } else {
-            result = y > 0 ? 2 : 3;
+            System.out.println(y > 0 ? 2 : 3);
         }
-        System.out.println(result);
     }
 }

216a2187311a57824446983bd5d6c9394a41b381
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-03-21 14:21:30 +0900

    [Bronze I] Title: 단어 공부, Time: 288 ms, Memory: 21080 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,5 +1,5 @@
 import java.io.*;
-
+import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,7 +11,7 @@ public class Main {
             alphabetCount[word.charAt(i) - 'A']++;
         }
 
-        int max = -1;
+        int max = 0;
         char result = '?';
         for (int i = 0; i < 26; i++) {
             if (max < alphabetCount[i]) {

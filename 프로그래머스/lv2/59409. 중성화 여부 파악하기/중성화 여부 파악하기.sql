3465fd52496913d4c8ed4b9a6447dd2761e4be3b
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-03-23 15:36:39 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -0,0 +1,6 @@
+-- 코드를 입력하세요
+SELECT ANIMAL_ID, NAME, 
+       CASE WHEN SUBSTRING_INDEX(SEX_UPON_INTAKE, ' ', 1) IN ('Neutered', 'Spayed') THEN 'O'
+            ELSE 'X' END AS "중성화"
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID

9cbe29cff71f6bc653017f56402a3be37d5af8e3
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-04-07 19:51:14 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+
+        for (int i = 0; i < n/2; i++) {
+            answer = answer.concat("수박");
+        }
+
+        if (n % 2 != 0) {answer = answer.concat("수");}
+
+        return answer;
+    }
+}

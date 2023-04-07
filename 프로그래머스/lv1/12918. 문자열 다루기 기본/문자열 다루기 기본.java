100432f436a4ba60ebab7ed01f98197e64e3e5d7
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-04-07 19:39:34 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.09 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,19 +1,5 @@
 class Solution {
     public boolean solution(String s) {
-        int len = s.length();
-        
-        if (len != 4 && len != 6) {
-            return false;
-        }
-        
-        char c;
-        for (int i = 0; i < len; i++) {
-            c = s.charAt(i);
-            if (c - '0' < 0 || c - '0' > 9) {
-                return false;
-            }
-        }
-        
-        return true;
+        return s.matches("\\d{4}|\\d{6}");
     }
 }

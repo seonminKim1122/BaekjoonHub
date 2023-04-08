37cd66cd2676766ce28d18f2ae1794b91c45a563
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-04-08 17:28:33 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -1,14 +1,17 @@
 class Solution {
     public boolean solution(int x) {
-        boolean answer = true;
-        String num = String.valueOf(x);
-        int divisor = 0;
+        int idx = 0;
+        int n = x;
+        while (x > 0) { // 6311
+            idx += (x % 10); // 1의 자리
+            x = x / 10; // 10 의자리
 
-        for(String n : num.split("")) {
-            divisor += Integer.parseInt(n);
         }
-
-        if ( x % divisor != 0) answer = false;
-        return answer;
+        
+        if (n % idx == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }

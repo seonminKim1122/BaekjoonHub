6736b892316bc49502f39f3657d4faa39f7f0c7a
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-04-08 15:08:03 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -3,10 +3,10 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        int divisor = 1;
-        while ((n % divisor) != n) {
-            answer += ((n % (divisor*10)) - (n % divisor)) / divisor;
-            divisor *= 10;
+        int divisor = 10;
+        while (n != 0) {
+            answer += (n % 10);
+            n /= 10;
         }
         return answer;
     }

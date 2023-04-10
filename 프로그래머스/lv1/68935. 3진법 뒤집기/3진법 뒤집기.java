f1a62e762c4dbc22fc3497df40e75a3520ae9a5f
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-04-10 16:15:04 +0900

    [level 1] Title: 3진법 뒤집기, Time: 10.81 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -3,17 +3,13 @@ import java.util.*;
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        Stack<Integer> t = new Stack<>();
-
-        while (n != 0){
-            t.add(n % 3);
+        String temp = "";
+        while(n > 0) {
+            temp += (n % 3);
             n /= 3;
         }
-        int len = t.size();
-        for (int i = 0; i < len; i++) {
-            answer += t.pop()*(int)Math.pow(3,i);
-        }
 
+        answer = Integer.parseInt(temp, 3);
         return answer;
     }
 }

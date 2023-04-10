94993d84742e5769be0c2697c7d6030cd906dbac
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-04-10 14:07:02 +0900

    [level 1] Title: 시저 암호, Time: 33.62 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -6,9 +6,9 @@ class Solution {
             if (c == ' ') {
                 answer += c;
             } else if (c - 'a' >= 0 && c - 'a' <= 26) {
-                answer += (char)((c - 'a' + n) % 26 + 97);
-            } else if (c - 'A' >= 0 && c - 'A' <= 26) {
-                answer += (char)((c - 'A' + n) % 26 + 65);
+                answer += (char)((c - 'a' + n) % 26 + 'a');
+            } else {
+                answer += (char)((c - 'A' + n) % 26 + 'A');
             }
         }
         return answer;

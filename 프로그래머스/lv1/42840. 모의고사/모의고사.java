7e5f55644a854ce620d89184413c9b715f71594f
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-04-10 16:56:40 +0900

    [level 1] Title: 모의고사, Time: 1.14 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -1,38 +1,40 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] answers) {        int[] answer = {};
-        List<Integer> tmp = new ArrayList<>();
-        int[] a = {1, 2, 3, 4, 5};
-        int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[][] patterns = {
+                {1, 2, 3, 4, 5},
+                {2, 1, 2, 3, 2, 4, 2, 5},
+                {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
 
-
-        int correctA = 0;
-        int correctB = 0;
-        int correctC = 0;
-
-        for (int i = 0; i < answers.length; i++) {
-            if (a[i%5] == answers[i])
-                correctA++;
-            if (b[i%8] == answers[i])
-                correctB++;
-            if (c[i%10] == answers[i])
-                correctC++;
+        int answersLength = answers.length;
+        int patternsLength = patterns.length;
+        int[] temp = new int[3];
+        for (int i = 0; i < answersLength; i++) {
+            for (int j = 0; j < patternsLength; j++) {
+                if (answers[i] == patterns[j][i % patterns[j].length]){
+                    temp[j]++;
+                }
+            }
         }
 
-        int max = Math.max(correctA, Math.max(correctB,correctC));
-        
-        if (max == correctA){
-            tmp.add(1);
+        int max = Math.max(Math.max(temp[0], temp[1]), temp[2]);
+        List<Integer> list = new ArrayList<>();
+        int i = 1;
+        for(int num : temp) {
+            if (max == num) {
+                list.add(i);
+            }
+            i++;
         }
-        if (max == correctB){
-            tmp.add(2);
+
+        i = 0;
+        int[] result = new int[list.size()];
+        for(int num : list) {
+            result[i++] = num;
         }
-        if (max == correctC){
-            tmp.add(3);
-        }
-        answer = tmp.stream().mapToInt(i -> i).toArray();
-        return answer;
+
+        return result;
         
     }
 }

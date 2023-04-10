52184d6ca3b0ac356b6f1763d068d7b54fbb27cf
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-04-10 17:48:44 +0900

    [level 1] Title: 시저 암호, Time: 27.60 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -1,14 +1,14 @@
 class Solution {
     public String solution(String s, int n) {
         String answer = "";
-        
+
         for(char c : s.toCharArray()) {
             if (c == ' ') {
                 answer += c;
-            } else if (c - 'a' >= 0 && c - 'a' <= 26) {
-                answer += (char)((c - 'a' + n) % 26 + 'a');
-            } else {
+            } else if (0 <= c-'A' && c-'A' <= 26) {
                 answer += (char)((c - 'A' + n) % 26 + 'A');
+            } else {
+                answer += (char)((c - 'a' + n) % 26 + 'a');
             }
         }
         return answer;

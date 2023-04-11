d30f0146565eb50525c745a7ce610a032a7443a6
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-04-11 11:06:30 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.32 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,14 +1,12 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[n];
+        int length = arr1.length;
+        String[] answer = new String[length];
 
-        for (int i = 0; i < n; i++) {
-            answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]).replaceAll("1", "#").replaceAll("0"," ");
-        }
-
-        for (int i = 0; i < n; i++) {
-            while (answer[i].length() < n){
-                answer[i] = " "+answer[i];
+        for (int i = 0; i < length; i++) {
+            answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]).replaceAll("1", "#").replaceAll("0", " ");
+            while(answer[i].length() < n) {
+                answer[i] = " " + answer[i];
             }
         }
 

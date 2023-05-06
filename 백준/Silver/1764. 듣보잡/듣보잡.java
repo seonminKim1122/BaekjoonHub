ad22bd41875d16c305d9b7db5228c432f2130bcd
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-05-06 15:25:59 +0900

    [Silver IV] Title: 듣보잡, Time: 420 ms, Memory: 26296 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            set.add(br.readLine());
+        }
+
+        int answer = 0;
+        List<String> answerList = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
+            String name = br.readLine();
+            if (set.contains(name)) {
+                answer++;
+                answerList.add(name);
+            }
+        }
+        System.out.println(answer);
+        answerList.stream().sorted().forEach(System.out::println);
+    }
+}

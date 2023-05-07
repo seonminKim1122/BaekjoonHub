1361fdbad55fd971bb18ad26000421e96572acd1
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-05-07 17:11:19 +0900

    [Silver III] Title: 분수 합, Time: 232 ms, Memory: 18392 KB -BaekjoonHub

---
+++ 백준/Silver/1735. 분수 합/분수 합.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        int e = a*d + b*c;
+        int f = b*d;
+        int gcd = gcd(e, f);
+
+        System.out.println(e / gcd + " " + f / gcd);
+    }
+
+    public static int gcd(int a, int b) {
+        if (a % b == 0) {
+            return b;
+        }
+        return gcd(b, a % b);
+    }
+}

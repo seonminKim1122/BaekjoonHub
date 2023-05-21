b647939254c0546d9e8bef4e827004c48efabf8b
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-05-21 18:08:46 +0900

    [Silver IV] Title: 덱, Time: 192 ms, Memory: 18056 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        Deque<Integer> deque = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            String order = br.readLine();
+            switch (order) {
+                case "pop_front":
+                    if (deque.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(deque.pollFirst()).append("\n");
+                    }
+                    break;
+                case "pop_back":
+                    if (deque.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(deque.pollLast()).append("\n");
+                    }
+                    break;
+                case "size":
+                    sb.append(deque.size()).append("\n");
+                    break;
+                case "empty":
+                    if (deque.isEmpty()) {
+                        sb.append(1).append("\n");
+                    } else {
+                        sb.append(0).append("\n");
+                    }
+                    break;
+                case "front":
+                    if (deque.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(deque.getFirst()).append("\n");
+                    }
+                    break;
+                case "back":
+                    if (deque.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(deque.getLast()).append("\n");
+                    }
+                    break;
+                default:
+                    String[] s = order.split(" ");
+                    if (s[0].equals("push_front")) {
+                        deque.offerFirst(Integer.parseInt(s[1]));
+                    } else {
+                        deque.offerLast(Integer.parseInt(s[1]));
+                    }
+            }
+        }
+        System.out.println(sb);
+    }
+}

0c373570bbf59640e116084f4e6b6000885b53bd
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-05-30 19:59:30 +0900

    [Silver II] Title: 랜선 자르기, Time: 184 ms, Memory: 17272 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    // Binary Search
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        int[] lans = new int[K];
+        long start = 0;
+        long end = 0;
+        for (int i = 0; i < K; i++) {
+            lans[i] = Integer.parseInt(br.readLine());
+            if (end < lans[i]) {
+                end = lans[i];
+            }
+        }
+
+        long result = 0;
+
+        while (start <= end) { // 정확히 N개를 맞출 수 없는 케이스도 분명 존재할 것
+            result = (start + end) / 2;
+
+            int count = 0;
+            
+            if (result == 0) {
+                result = 1;
+            }
+
+            for (int lan : lans) {
+                count += lan / result;
+            }
+
+            if (count < N) {
+                end = result - 1;
+            } else {
+                start = result + 1;
+            }
+        }
+        System.out.println(start - 1);
+    }
+}

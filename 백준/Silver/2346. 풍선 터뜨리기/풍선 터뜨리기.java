0a3c8d0d13101ad9ec43f37ebd7983b0bc49d96f
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-07-17 10:42:09 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 172 ms, Memory: 14816 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -22,21 +21,23 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         while (true) {
             int[] arr = deque.pollFirst();
-
+            
+            int num = arr[0];
+            int move = arr[1];
             // 터진 풍선의 번호 StringBuilder 에 추가
-            sb.append(arr[0]).append(" ");
+            sb.append(num).append(" ");
 
             if (deque.isEmpty()) {
                 break;
             }
 
             // 풍선 안에 담긴 숫자대로 이동
-            if (arr[1] < 0) {
-                for (int i = arr[1]; i < 0; i++) {
+            if (move < 0) {
+                for (int i = move; i < 0; i++) {
                     deque.addFirst(deque.pollLast());
                 }
             } else {
-                for (int i = 0; i < arr[1]-1; i++) {
+                for (int i = 0; i < move-1; i++) {
                     deque.addLast(deque.pollFirst());
                 }
             }

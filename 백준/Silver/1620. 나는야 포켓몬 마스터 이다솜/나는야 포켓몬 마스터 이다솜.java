1a139beebbd7ad310c65af7faad1bd1d0ddddd36
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-07-22 09:19:44 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 528 ms, Memory: 59604 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -13,20 +13,35 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        Map<String, String> map = new HashMap<>();
-
+        Map<String, Integer> map1 = new HashMap<>();
+        Map<Integer, String> map2 = new HashMap<>();
         for (int i = 0; i < N; i++) {
             String pokemon = br.readLine();
-            map.put(pokemon, String.valueOf(i+1));
-            map.put(String.valueOf(i+1), pokemon);
+            map1.put(pokemon, i+1);
+            map2.put(i+1, pokemon);
         }
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
             String input = br.readLine();
-            sb.append(map.get(input)).append("\n");
+            if (canBeInteger(input)) {
+                sb.append(map2.get(Integer.parseInt(input))).append("\n");
+            } else {
+                sb.append(map1.get(input)).append("\n");
+            }
         }
 
         System.out.println(sb);
+    }
+    
+    public static boolean canBeInteger(String input) {
+
+        for (char c : input.toCharArray()) {
+            if (c < '0' || c > '9') {
+                return false;
+            }
+        }
+        
+        return true;
     }
 }

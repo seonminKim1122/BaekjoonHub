4746932c14911a76b54a173a7b513a30bafd973c
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-07-22 09:58:55 +0900

    [Silver II] Title: 최대 힙, Time: 276 ms, Memory: 23164 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -1,73 +1,86 @@
-import java.io.*;
-public class Main {
-    static int[] arr = new int[100001]; // 0번째 인덱스는 안 쓸 거니까
-    static int top = 0; // 힙의 크기
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
 
+        MaxHeap maxHeap = new MaxHeap(N);
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
             int x = Integer.parseInt(br.readLine());
 
-            if (x == 0) {
-                if (top == 0) {
-                    bw.write(0 + "\n");
-                } else {
-                    bw.write(pop() + "\n");
-                }
-            } else {
-                push(x);
+            if (x > 0) {
+                maxHeap.add(x);
+            } else if (x == 0) {
+                sb.append(maxHeap.pop()).append("\n");
             }
         }
 
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(sb);
+    }
+}
 
+class MaxHeap {
+    int[] arr;
+    int last = 0;
+
+    MaxHeap(int n) {
+        arr = new int[n+1];
     }
 
-    public static void push(int x) {
-        // 1. 배열에 값을 추가
-        arr[++top] = x;
+    void add(int x) {
+        arr[++last] = x;
 
-        // 2. 부모 노드와 비교
-        int idx = top;
+        int self = last;
+        int parent = self / 2;
 
-        while (arr[idx/2] < arr[idx] && idx > 1) { // 부모 노드가 존재하는데 자식 노드보다 크면
-            // 서로 자리 바꾸기
-            int parent = arr[idx/2];
-            arr[idx/2] = arr[idx];
-            arr[idx] = parent;
-            idx = idx / 2;
+        while (arr[parent] < arr[self] && self > 1) {
+            int temp = arr[parent];
+            arr[parent] = arr[self];
+            arr[self] = temp;
+
+            self = parent;
+            parent = self / 2;
         }
     }
 
-    public static int pop() {
-        // 1. 배열에서 가장 큰 값을 출력
+    int pop() {
+        if (last == 0) {
+            return 0;
+        }
+
         int result = arr[1];
+        arr[1] = arr[last];
+        arr[last--] = 0;
 
-        // 2. 그 값을 배열에서 제거
-        arr[1] = arr[top];
-        arr[top--] = 0;
+        int self = 1;
+        int leftChild = 2 * self;
+        int rightChild = 2 * self + 1;
 
-        // 3. 배열이 최대힙구조 유지할 수 있도록 재정렬
-        for(int idx = 1; idx <= top/2;) {  // 자식 노드가 존재할 때
-            if (arr[idx] >= Math.max(arr[2*idx], arr[2*idx+1])) {break;} // 부모가 자식보다 크면 탈출
-            if (arr[2*idx] > arr[2*idx+1]) {
-                int left = arr[2*idx];
-                arr[2*idx] = arr[idx];
-                arr[idx] = left;
-                idx = 2 * idx;
-            } else { // 그렇지 않으면
-                int right = arr[2*idx+1];
-                arr[2*idx+1] = arr[idx];
-                arr[idx] = right;
-                idx = 2 * idx + 1;
+        while (Math.max(arr[leftChild], arr[rightChild]) > arr[self]) {
+            int temp = arr[self];
+            if (arr[leftChild] < arr[rightChild]) {
+                arr[self] = arr[rightChild];
+                arr[rightChild] = temp;
+                self = rightChild;
+            } else {
+                arr[self] = arr[leftChild];
+                arr[leftChild] = temp;
+                self = leftChild;
+            }
+
+            leftChild = 2 * self;
+            rightChild = 2 * self + 1;
+
+            if (leftChild > last) {
+                break;
             }
         }
+
         return result;
     }
 }

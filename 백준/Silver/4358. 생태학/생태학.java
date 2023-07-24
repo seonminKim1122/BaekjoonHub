f6c542879632f5c165b7ef64e03ccc5fc1438f1a
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-07-24 09:46:45 +0900

    [Silver II] Title: 생태학, Time: 732 ms, Memory: 118696 KB -BaekjoonHub

---
+++ 백준/Silver/4358. 생태학/생태학.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Map<String, Double> map = new HashMap<>();
+        int count = 0;
+        while (true) {
+            String tree = br.readLine();
+
+            if (tree == null || tree.equals("")) break;
+
+            map.put(tree, map.getOrDefault(tree, 0d) + 1);
+            count++;
+        }
+
+        List<String> trees = new ArrayList<>(map.keySet());
+        trees.sort(String::compareTo);
+
+        StringBuilder sb = new StringBuilder();
+        for (String tree : trees) {
+            double ratio = 100 * ((double) map.get(tree) / count);
+            sb.append(tree).append(" ").append(String.format("%.4f", ratio)).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}

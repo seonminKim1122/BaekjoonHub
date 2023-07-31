fcc256be063ef2dbb4c6898733875cdca112d29f
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-07-31 09:12:24 +0900

    [Silver IV] Title: 동전 0, Time: 128 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] coins = new int[N];
+        for (int i = 0; i < N; i++) {
+            int coin = Integer.parseInt(br.readLine());
+            coins[(N-1)-i] = coin;
+        }
+
+        int result = 0;
+        int i = 0;
+        while (K != 0) {
+            int coin = coins[i++];
+            int cnt = K / coin;
+            result += cnt;
+            K -= coin * (cnt);
+        }
+
+        System.out.println(result);
+    }
+}

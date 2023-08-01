cc62f7c2ce2654bea1a6ba1bfef5ef9eae96dcd4
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-08-01 10:24:17 +0900

    [Gold V] Title: 행복 유치원, Time: 640 ms, Memory: 57108 KB -BaekjoonHub

--- 백준/Gold/13164. 행복 유치원/행복 유치원.java
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.java
@@ -12,10 +12,10 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
         int[] heights = new int[N];
         for (int i = 0; i < N; i++) {
-            heights[i] = Integer.parseInt(st2.nextToken());
+            heights[i] = Integer.parseInt(st.nextToken());
         }
 
         int[] diffs = new int[N-1];
@@ -26,7 +26,7 @@ public class Main {
         Arrays.sort(diffs);
 
         int result = 0;
-        for (int i = 0; i < N-K; i++) {
+        for (int i = 0; i < N-K; i++) { // 1번 합치면 그룹이 N-1 개 가 됌. K개 그룹이 되기 위해서는 N-K 번 합쳐야 함
             result += diffs[i];
         }
 

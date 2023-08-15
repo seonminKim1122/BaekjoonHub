74f714a354d1146b8c5170455412208809ea5b11
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-08-15 10:46:10 +0900

    [Silver I] Title: 숨바꼭질, Time: 160 ms, Memory: 19748 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,40 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int K = sc.nextInt();
-        // BFS 로 문제 풀기
-        boolean[] visited = new boolean[1000001];
+        int from = Integer.parseInt(st.nextToken());
+        int to = Integer.parseInt(st.nextToken());
+
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{N, 0});
-        int result = 0;
-        while (!queue.isEmpty()) {
-            int X = queue.peek()[0];
-            int depth = queue.peek()[1];
+        boolean[] visited = new boolean[1000001];
 
-            if (X == K) {
-                result = depth;
+        int result = 0;
+        queue.add(new int[]{from, 0});
+        visited[from] = true;
+
+        while (!queue.isEmpty()) {
+            int[] temp = queue.poll();
+            int now = temp[0];
+            int sec = temp[1];
+            if (now == to) {
+                result = sec;
                 break;
             }
-            if (X-1 >= 0 && !visited[X-1]) {
-                queue.add(new int[]{X-1, depth+1});
+
+            if (now < 50001 && !visited[2 * now]) {
+                queue.add(new int[]{2 * now, sec + 1});
+                visited[2 * now] = true;
             }
 
-            if (X+1 <= 100000 && !visited[X+1]) {
-                queue.add(new int[]{X+1, depth+1});
+            if (now >= 1 && !visited[now - 1]) {
+                queue.add(new int[]{now - 1, sec + 1});
+                visited[now - 1] = true;
             }
 
-            if (2*X <= 100000 && !visited[2*X]) {
-                queue.add(new int[]{2*X, depth+1});
+            if (now < 100000 && !visited[now + 1]) {
+                queue.add(new int[]{now + 1, sec + 1});
+                visited[now + 1] = true;
             }
 
-            queue.remove();
-            visited[X] = true;
         }
 
         System.out.println(result);

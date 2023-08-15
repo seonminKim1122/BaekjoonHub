75abf4754b0c7d4353496a71d3b4d7e4c2f3aa7d
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-08-15 09:19:23 +0900

    [Silver I] Title: 미로 탐색, Time: 192 ms, Memory: 16568 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,8 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
+import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -13,40 +13,53 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[][] miro = new int[N][M];
-        boolean[][] visited = new boolean[N][M];
-        int[][] distances = new int[N][M];
-
+        int[][] maze = new int[N][M];
         for (int i = 0; i < N; i++) {
-            String[] temp = br.readLine().split("");
+            String[] input = br.readLine().split("");
             for (int j = 0; j < M; j++) {
-                miro[i][j] = Integer.parseInt(temp[j]);
+                maze[i][j] = Integer.parseInt(input[j]);
             }
         }
 
+        boolean[][] visited = new boolean[N][M];
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0, 0});
+
+        queue.add(new int[]{0, 0, 1});
         visited[0][0] = true;
-        distances[0][0] = 1;
 
-        int[] dx = {0, 0, -1, 1};
-        int[] dy = {-1, 1, 0, 0};
-
-        int result = 0;
+        int result = 1;
         while (!queue.isEmpty()) {
-            int[] temp = queue.remove();
-            for (int i = 0; i < 4; i++) {
-                int x = temp[0] + dx[i];
-                int y = temp[1] + dy[i];
+            int[] point = queue.poll();
+            int i = point[0];
+            int j = point[1];
+            int block = point[2];
 
-                if (x < 0 || y < 0 || x >= N || y >= M || visited[x][y] || miro[x][y] == 0) continue;
+            if (i == N-1 && j == M-1) {
+                result = block;
+            }
 
-                queue.add(new int[]{x, y});
-                visited[x][y] = true;
-                distances[x][y] = distances[temp[0]][temp[1]] + 1;
+            // 상, 하, 좌, 우
+            if (i-1 >= 0 && !visited[i-1][j] && maze[i-1][j] != 0) {
+                queue.add(new int[]{i-1, j, block+1});
+                visited[i-1][j] = true;
+            }
+
+            if (i+1 < N && !visited[i+1][j] && maze[i+1][j] != 0) {
+                queue.add(new int[]{i+1, j, block+1});
+                visited[i+1][j]= true;
+            }
+
+            if (j-1 >= 0 && !visited[i][j-1] && maze[i][j-1] != 0) {
+                queue.add(new int[]{i, j-1, block+1});
+                visited[i][j-1] = true;
+            }
+
+            if (j+1 < M && !visited[i][j+1] && maze[i][j+1] != 0) {
+                queue.add(new int[]{i, j+1, block+1});
+                visited[i][j+1] = true;
             }
         }
 
-        System.out.println(distances[N-1][M-1]);
+        System.out.println(result);
     }
 }

c264b3978789910c5a54adb13ef357d9e14c5823
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-08-18 10:07:21 +0900

    [Silver III] Title: Four Squares, Time: 172 ms, Memory: 14600 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.java
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -1,23 +1,26 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
         int[] dp = new int[n+1];
         dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
+        for (int i = 2; i < n+1; i++) {
             int sqrt = (int)Math.sqrt(i);
             if (Math.sqrt(i) == sqrt) {
                 dp[i] = 1;
             } else {
                 dp[i] = dp[i-1] + dp[1];
                 for (int j = 2; j <= sqrt; j++) {
-                    dp[i] = Math.min(dp[i - (j*j)] + dp[j*j], dp[i]);
+                    dp[i] = Math.min(dp[i - j*j] + dp[j*j], dp[i]);
                 }
             }
         }
+
         System.out.println(dp[n]);
     }
 }

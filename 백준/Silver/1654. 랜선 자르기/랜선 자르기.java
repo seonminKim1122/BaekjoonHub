5690a6dc0e0887b8432ee9c001d0375a141fdd79
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-08-18 09:36:18 +0900

    [Silver II] Title: 랜선 자르기, Time: 176 ms, Memory: 17528 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -4,15 +4,15 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    // Binary Search
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int K = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
+
+        int K = Integer.parseInt(st.nextToken()); // 가지고 있는 랜선의 개수
+        int N = Integer.parseInt(st.nextToken()); // 필요한 랜선의 개수
 
         int[] lans = new int[K];
-        long start = 0;
+        long start = 1;
         long end = 0;
         for (int i = 0; i < K; i++) {
             lans[i] = Integer.parseInt(br.readLine());
@@ -21,23 +21,22 @@ public class Main {
             }
         }
 
-        long result = 0;
+        // N 개를 만들 수 있는 랜선의 최대 길이를 구하라
+        while (start <= end) {
+            long mid = (start + end) / 2;
 
-        while (start <= end) { // 정확히 N개를 맞출 수 없는 케이스도 분명 존재할 것
-            result = (long) Math.ceil((double)(start + end) / 2);
-
-            int count = 0;
-            
-            for (int lan : lans) {
-                count += lan / result;
+            int cnt = 0;
+            for (int i = 0; i < K; i++) {
+                cnt += (lans[i] / mid);
             }
 
-            if (count < N) {
-                end = result - 1;
-            } else {
-                start = result + 1;
+            if (cnt < N) {
+                end = mid - 1;
+            } else  {
+                start = mid + 1; // == N 도 포함시킨 이유는 == N이 되는 최대를 찾아야 하기 때문
             }
         }
-        System.out.println(start - 1);
+
+        System.out.println(end);
     }
 }

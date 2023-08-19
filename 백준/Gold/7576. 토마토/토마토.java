dd5dcfad9daf74b4b7c3bfba2e76a75b5f40987f
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-08-19 10:01:16 +0900

    [Gold V] Title: 토마토, Time: 676 ms, Memory: 129056 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -2,79 +2,59 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
+import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int M = Integer.parseInt(st.nextToken()); // 가로
-        int N = Integer.parseInt(st.nextToken()); // 세로
 
-        int[][] tomatos = new int[N][M];
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        int[][] box = new int[N][M];
         Queue<int[]> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[N][M];
 
         for (int i = 0; i < N; i++) {
-            StringTokenizer temp = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                tomatos[i][j] = Integer.parseInt(temp.nextToken());
-                if (tomatos[i][j] == 1) {
+                box[i][j] = Integer.parseInt(st.nextToken());
+                if (box[i][j] == 1) {
                     queue.add(new int[]{i, j, 0});
+                    visited[i][j] = true;
                 }
             }
         }
 
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
         int result = 0;
         while (!queue.isEmpty()) {
-            int i = queue.peek()[0];
-            int j = queue.peek()[1];
-            int depth = queue.peek()[2];
-            
-            if (check(i, j-1, tomatos)) { // 상
-                tomatos[i][j-1] = 1;
-                queue.add(new int[]{i, j-1, depth+1});
-            }
+            int[] temp = queue.poll();
 
-            if (check(i, j+1, tomatos)) { // 하
-                tomatos[i][j+1] = 1;
-                queue.add(new int[]{i, j+1, depth+1});
-            }
+            for (int i = 0; i < 4; i++) {
+                int x = temp[0] + dx[i];
+                int y = temp[1] + dy[i];
 
-            if (check(i-1, j, tomatos)) { // 좌
-                tomatos[i-1][j] = 1;
-                queue.add(new int[]{i-1, j, depth+1});
-            }
+                if (x < 0 || y < 0 || x >= N || y >= M || visited[x][y] || box[x][y] != 0) continue;
 
-            if (check(i+1, j, tomatos)) { // 우
-                tomatos[i+1][j] = 1;
-                queue.add(new int[]{i+1, j, depth+1});
+                queue.add(new int[]{x, y, temp[2]+1});
+                visited[x][y] = true;
+                result = temp[2] + 1;
             }
-            queue.remove();
-            result = depth;
         }
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if (tomatos[i][j] == 0) {
-                    result = -1;
-                    break;
+                if (box[i][j] != -1 && !visited[i][j]) {
+                    System.out.println(-1);
+                    return;
                 }
             }
         }
 
         System.out.println(result);
-    }
-
-    public static boolean check(int i, int j, int[][] tomatos) {
-        if (i < 0 || i >= tomatos.length || j < 0 || j >= tomatos[0].length) {
-            return false;
-        }
-
-        if (tomatos[i][j] == 0) {
-            return true;
-        }
-
-        return false;
     }
 }

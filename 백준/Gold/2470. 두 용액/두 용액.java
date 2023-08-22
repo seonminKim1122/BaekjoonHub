9d671431ea1e8b696f7a2fae5a4363d3b0979afa
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-08-22 10:18:07 +0900

    [Gold V] Title: 두 용액, Time: 408 ms, Memory: 28480 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] values = new int[N];
+        for (int i = 0; i < N; i++) {
+            values[i] = Integer.parseInt(st.nextToken()); // 산성 1 ~ 10억, 알칼리 -1 ~ -10억
+        }
+
+        Arrays.sort(values);
+
+        int start = 0;
+        int end = N-1;
+        int tempValue = values[start] + values[end];
+        int[] result = new int[]{values[start], values[end]};
+
+        while (start < end) {
+            int mixedValue = values[start] + values[end];
+
+            if (Math.abs(mixedValue) <= Math.abs(tempValue)) {
+                tempValue = values[start] + values[end];
+                result[0] = values[start];
+                result[1] = values[end];
+            }
+
+            if (mixedValue > 0) {
+                end--;
+            } else {
+                start++;
+            }
+        }
+
+        System.out.println(result[0] + " " + result[1]);
+    }
+}

5b53771691ecaaa956437f7f480d8c432c918815
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-09-22 09:10:04 +0900

    [Silver III] Title: Four Squares, Time: 152 ms, Memory: 14508 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.java
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -8,15 +8,15 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
 
         int[] dp = new int[n+1];
-        dp[1] = 1;
-        for (int i = 2; i < n+1; i++) {
+
+        for (int i = 1; i <= n; i++) {
             double sqrt = Math.sqrt(i);
             if ((int)sqrt == sqrt) {
                 dp[i] = 1;
             } else {
-                dp[i] = dp[i-1] + dp[1];
+                dp[i] = dp[i - 1] + dp[1];
                 for (int j = 2; j <= sqrt; j++) {
-                    dp[i] = Math.min(dp[i - j*j] + dp[j*j], dp[i]);
+                    dp[i] = Math.min(dp[i - j * j] + dp[j * j], dp[i]);
                 }
             }
         }

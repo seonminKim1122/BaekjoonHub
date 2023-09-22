19b2d3916d569f79912e05f2803d635d971e8ae6
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-09-22 09:31:15 +0900

    [Silver III] Title: 1로 만들기, Time: 128 ms, Memory: 15276 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,25 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        int[] dp = new int[N+1];
+        boolean[] visited = new boolean[N + 1];
+        
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{N, 0});
+        visited[N] = true;
 
-        for (int i = 2; i <= N; i++) {
-            dp[i] = dp[i - 1] + 1;
+        while (!queue.isEmpty()) {
+            int[] temp = queue.poll();
+            int now = temp[0];
+            int cnt = temp[1];
 
-            if (i % 3 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            if (now == 1) {
+                System.out.println(cnt);
+                break;
             }
 
-            if (i % 2 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            if (now % 3 == 0 && !visited[now / 3]) {
+                queue.add(new int[]{now / 3, cnt + 1});
+                visited[now / 3] = true;
+            }
+
+            if (now % 2 == 0 && !visited[now / 2]) {
+                queue.add(new int[]{now / 2, cnt + 1});
+                visited[now / 2] = true;
+            }
+
+            if (!visited[now - 1]) {
+                queue.add(new int[]{now - 1, cnt + 1});
+                visited[now - 1] = true;
             }
         }
-        System.out.println(dp[N]);
     }
 }

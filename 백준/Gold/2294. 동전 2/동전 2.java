802a95ee4dbe12f67c01078b0dae3616dc20afe0
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-10-06 09:58:35 +0900

    [Gold V] Title: 동전 2, Time: 140 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[k + 1];
+
+        for (int i = 0; i < n; i++) {
+            int coin = coins[i];
+
+            for (int j = 1; j <= k; j++) {
+                if (i == 0) {
+                    dp[j] = 100001;
+                }
+
+                if(j - coin >= 0) {
+                    dp[j] = Math.min(dp[j], dp[j - coin] + 1);
+                }
+            }
+        }
+
+        System.out.println(dp[k] == 100001 ? -1 : dp[k]);
+    }
+}

65ef73a1630d956410bdd4980e2139be1a9f4b2f
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-10-09 10:48:52 +0900

    [Gold V] Title: LCS, Time: 152 ms, Memory: 18236 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char[] s1 = br.readLine().toCharArray();
+        char[] s2 = br.readLine().toCharArray();
+
+        int R = s1.length;
+        int C = s2.length;
+
+        int[][] dp = new int[R + 1][C + 1];
+        for (int i = 1; i <= R; i++) {
+            for (int j = 1; j <= C; j++) {
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        System.out.println(dp[R][C]);
+    }
+}

ec376e77fdcdabaaf3850c39ded4d214f2a93a6e
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-10-30 22:08:07 +0900

    [Silver II] Title: 1, 2, 3 더하기 5, Time: 156 ms, Memory: 18088 KB -BaekjoonHub

---
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[][] dp = new int[100001][4];
+        dp[1][1] = 1;
+        dp[2][2] = 1;
+        dp[3][1] = 1;
+        dp[3][2] = 1;
+        dp[3][3] = 1;
+
+        for (int i = 4; i <= 100000; i++) {
+            dp[i][1] = (dp[i - 1][2] + dp[i - 1][3]) % 1_000_000_009;
+            dp[i][2] = (dp[i - 2][1] + dp[i - 2][3]) % 1_000_000_009;
+            dp[i][3] = (dp[i - 3][1] + dp[i - 3][2]) % 1_000_000_009;
+        }
+
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < T; i++) {
+            int n = Integer.parseInt(br.readLine());
+            sb.append(((long) dp[n][1] + dp[n][2] + dp[n][3]) % 1_000_000_009).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}

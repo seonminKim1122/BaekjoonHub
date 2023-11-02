a949c365c3873620cc069fdff958f5b8894f4a0e
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-11-02 11:10:52 +0900

    [Silver I] Title: 볼 모으기, Time: 260 ms, Memory: 19080 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17615. 볼 모으기/볼 모으기.java
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.java
@@ -9,25 +9,38 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         String input = br.readLine();
 
-        char[] balls = new char[N];
-
-        int cntOfRed = 0;
-        int cntOfBlue = 0;
+        int cntOfRed1 = 0;
+        int cntOfBlue1 = 0;
 
         int redBeforeLastBlue = 0;
         int blueBeforeLastRed = 0;
 
+        int cntOfRed2 = 0;
+        int cntOfBlue2 = 0;
+
+        int redAfterFirstBlue = 0;
+        int blueAfterFirstRed = 0;
+
         for (int i = 0; i < N; i++) {
-            balls[i] = input.charAt(i);
-            if (balls[i] == 'R') {
-                cntOfRed++;
-                blueBeforeLastRed = cntOfBlue;
+            char ball = input.charAt(i);
+            if (ball == 'R') {
+                cntOfRed1++;
+                blueBeforeLastRed = cntOfBlue1;
             } else {
-                cntOfBlue++;
-                redBeforeLastBlue = cntOfRed;
+                cntOfBlue1++;
+                redBeforeLastBlue = cntOfRed1;
+            }
+
+            ball = input.charAt(N - 1 - i);
+            if (ball == 'R') {
+                cntOfRed2++;
+                blueAfterFirstRed = cntOfBlue2;
+            } else {
+                cntOfBlue2++;
+                redAfterFirstBlue = cntOfRed2;
             }
         }
 
-        System.out.println(Math.min(redBeforeLastBlue, blueBeforeLastRed));
+        System.out.println(Math.min(Math.min(Math.min(redAfterFirstBlue, blueAfterFirstRed), redBeforeLastBlue), blueBeforeLastRed));
     }
 }

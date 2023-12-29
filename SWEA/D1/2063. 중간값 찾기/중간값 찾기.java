ac5e8dc062f6d8d8e1ec258c7db667095d966ee8
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-12-30 00:23:43 +0900

    [D1] Title: 중간값 찾기, Time: 134 ms, Memory: 20,724 KB -BaekjoonHub

--- SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
+++ SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
@@ -9,13 +9,12 @@ class Solution
 		Scanner sc = new Scanner(System.in);
 		
         int N = sc.nextInt();
-        
         int[] numbers = new int[N];
         for (int i = 0; i < N; i++) {
         	numbers[i] = sc.nextInt();
         }
+		Arrays.sort(numbers);
         
-        Arrays.sort(numbers);
         System.out.println(numbers[N / 2]);
 	}
 }

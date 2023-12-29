2281cb22630cc1971cee6cfb38c019e851701572
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-12-30 00:26:27 +0900

    [D1] Title: 자릿수 더하기, Time: 111 ms, Memory: 17,944 KB -BaekjoonHub

--- SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.java
+++ SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.java
@@ -1,20 +1,22 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.io.FileInputStream;
 
 class Solution
 {
 	public static void main(String args[]) throws Exception
 	{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		Scanner sc = new Scanner(System.in);
+		
+        String strNum = sc.next();
         
-		String[] numbers = br.readLine().split("");
-        int len = numbers.length;
+        int result = 0;
+        int len = strNum.length();
         
-        int answer = 0;
         for (int i = 0; i < len; i++) {
-        	answer += Integer.parseInt(numbers[i]);
+        	result += (strNum.charAt(i) - '0');
         }
         
-        System.out.println(answer);
+        System.out.println(result);
 	}
 }

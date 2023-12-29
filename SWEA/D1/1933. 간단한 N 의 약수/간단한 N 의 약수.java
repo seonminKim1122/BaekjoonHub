385aead1595bcec419a66d5e1b4e326acbe7e5c6
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-12-29 21:40:34 +0900

    [D1] Title: 간단한 N 의 약수, Time: 133 ms, Memory: 20,328 KB -BaekjoonHub

---
+++ SWEA/D1/1933. 간단한 N 의 약수/간단한 N 의 약수.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+        StringBuilder answer = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+        	if (N % i == 0) answer.append(i).append(" ");
+        }
+        
+        System.out.println(answer);
+	}
+}

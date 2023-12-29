cd05a83f227c544e84743a081f18235007b2c75d
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2023-12-29 20:08:28 +0900

    [D1] Title: 최대수 구하기, Time: 148 ms, Memory: 20,908 KB -BaekjoonHub

---
+++ SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		
+        StringBuilder answer = new StringBuilder();
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+            
+            
+			int max = -1;
+            for (int i = 0; i < 10; i++) {
+            	int num = sc.nextInt();
+                max = Math.max(max, num);
+            }
+            
+            answer.append("#").append(test_case).append(" ").append(max).append("\n");
+		}
+        System.out.println(answer);
+	}
+}

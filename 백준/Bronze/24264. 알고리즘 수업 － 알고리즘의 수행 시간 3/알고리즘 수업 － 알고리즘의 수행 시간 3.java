e818e5b131e4542a8062ad9e7096598b28201b43
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2024-01-22 21:57:39 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 124 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -1,10 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        long n = sc.nextLong();
-        System.out.println(n*n);
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println((long)n * n);
         System.out.println(2);
     }
 }
+/*
+시간복잡도 : O(N^2)
+
+실제로 메서드를 구현하고 count 변수를 통해 코드 호출 횟수를 세는 방식은
+진짜 O(N^2) 만큼 소요되므로 n = 500,000 일 때는 엄청난 횟수의 연산을 필요로 한다.
+
+이렇게 되면 시간 제한인 1초 안에 통과할 수 없으므로 실제 구현은 하지 않는다.
+ */

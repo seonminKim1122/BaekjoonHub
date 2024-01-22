a3e31d7c73d63b36834689d99df3e2199de03b8b
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2024-01-22 22:39:36 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 128 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.java
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.java
@@ -1,14 +1,20 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a1 = sc.nextInt();
-        int a0 = sc.nextInt();
-        int c = sc.nextInt();
-        int n0 = sc.nextInt();
-        
-        if (a0 <= n0*(c-a1) && (c-a1) >= 0) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a1 = Integer.parseInt(st.nextToken());
+        int a0 = Integer.parseInt(st.nextToken());
+
+        int c = Integer.parseInt(br.readLine());
+        int n0 = Integer.parseInt(br.readLine());
+
+        if ((c - a1 >= 0) && (c - a1) * n0 >= a0) {
             System.out.println(1);
         } else {
             System.out.println(0);

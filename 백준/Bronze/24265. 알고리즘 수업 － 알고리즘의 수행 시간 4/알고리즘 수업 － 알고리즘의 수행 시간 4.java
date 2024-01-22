fefed9fd60f1539dc4579168240d001af9e66ff6
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2024-01-22 22:03:06 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 120 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -1,11 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        long n = sc.nextInt();
-        
-        System.out.println(n*(n-1)/2);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        System.out.println((long) n * (n - 1) / 2);
         System.out.println(2);
     }
 }
+/*
+1 <= i <= n-1
+i + 1 <= j <= n
+
+2 <= <= n
+3 <= <= n
+...
+n <= <= n
+
+1 + 2 + 3 + 4 + ... + n-1
+=> (n-1)*n / 2
+ */

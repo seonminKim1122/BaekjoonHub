d71b4ddea099872d176d50f0c48eeccacbbd0ada
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2024-01-22 21:51:31 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 124 ms, Memory: 15976 KB -BaekjoonHub

--- 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -1,9 +1,31 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        System.out.println(n);
+
+    static int count = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] A = new int[n + 1];
+        MenOfPassion(A, n);
+
+        System.out.println(count);
         System.out.println(1);
     }
+
+    public static int MenOfPassion(int[] A, int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += A[i]; // 코드1
+            count++;
+        }
+        return sum;
+    }
 }
+/*
+다항식으로 나타내면 N 이므로 최고 차항은 1
+ */

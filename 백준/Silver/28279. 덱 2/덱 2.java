1eda7a7fe9528bc586099e0aac758de9b4efc01e
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2024-01-23 23:09:14 +0900

    [Silver IV] Title: 덱 2, Time: 1060 ms, Memory: 255220 KB -BaekjoonHub

--- 백준/Silver/28279. 덱 2/덱 2.java
+++ 백준/Silver/28279. 덱 2/덱 2.java
@@ -1,52 +1,54 @@
-import java.io.*;
-import java.util.ArrayDeque;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
-
 public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static Deque<Integer> deque = new ArrayDeque<>();
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        int N = Integer.parseInt(br.readLine()); //명령어의 수
+        StringBuilder answer = new StringBuilder();
+        Deque<Integer> deque = new LinkedList<>();
 
-        while(N --> 0) {
-            st = new StringTokenizer(br.readLine());
-            int order = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            StringTokenizer orders = new StringTokenizer(br.readLine());
+            int order = Integer.parseInt(orders.nextToken());
 
-            switch (order){
-                case 1 : //명령어 1 X : 덱의 앞에 넣는다
-                    deque.addFirst(Integer.parseInt(st.nextToken()));
+            switch (order) {
+                case 1:
+                    deque.addFirst(Integer.parseInt(orders.nextToken()));
                     break;
-                case 2 : //명령어 2 X : 덱의 뒤에 넣는다
-                    deque.addLast(Integer.parseInt(st.nextToken()));
+                case 2:
+                    deque.addLast(Integer.parseInt(orders.nextToken()));
                     break;
-                case 3 : //명령어 3 : 덱의 맨 앞의 정수를 빼서 출력한다.(없다면 -1)
-                    sb.append(deque.isEmpty()? -1 : deque.pollFirst()).append("\n");
+                case 3:
+                    answer.append(deque.isEmpty() ? -1 : deque.pollFirst()).append("\n");
                     break;
-                case 4 : //명령어 4 : 덱의 맨 뒤의 정수를 빼서 출력한다.(없다면 -1)
-                    sb.append(deque.isEmpty()? -1 : deque.pollLast()).append("\n");
+                case 4:
+                    answer.append(deque.isEmpty() ? -1 : deque.pollLast()).append("\n");
                     break;
-                case 5 : //명령어 5 : 덱에 들어있는 정수의 개수 출력
-                    sb.append(deque.size()).append("\n");
+                case 5:
+                    answer.append(deque.size()).append("\n");
                     break;
-                case 6 : //명령어 6 : 덱이 비어있으면 1 출력 비어있지 않으면 0
-                    sb.append(deque.isEmpty()? 1 : 0).append("\n");
+                case 6:
+                    answer.append(deque.isEmpty() ? 1 : 0).append("\n");
                     break;
-                case 7 : //명령어 7 : 덱에 정수가 있다면 맨 앞의 정수를 출력(없다면 -1)
-                    sb.append(deque.isEmpty()? -1 :deque.peekFirst()).append("\n");
+                case 7:
+                    answer.append(deque.isEmpty() ? -1 : deque.peekFirst()).append("\n");
                     break;
-                case 8 : //명령어 8 : 덱에 정수가 있다면 맨 뒤의 정수를 출력(없다면 -1)
-                    sb.append(deque.isEmpty()? -1 : deque.peekLast()).append("\n");
-                    break;
+                default:
+                    answer.append(deque.isEmpty() ? -1 : deque.peekLast()).append("\n");
             }
         }
-        br.close();
-        System.out.println(sb);
-    }
 
+        System.out.println(answer);
+    }
 }
+/*
+시간복잡도 : O(N)
+-> 덱 자료구조의 경우 삽입, 삭제 연산은 O(1) 이므로 명령의 갯수가 전체 코드의 시간복잡도를 결정
+ */

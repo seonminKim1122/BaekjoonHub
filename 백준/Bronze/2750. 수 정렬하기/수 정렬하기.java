4af110a4452c9389887d7c5782215503b66eee82
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2024-01-24 21:12:27 +0900

    [Bronze II] Title: 수 정렬하기, Time: 144 ms, Memory: 14416 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -8,17 +8,39 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-
+        int[] numbers = new int[N];
         for (int i = 0; i < N; i++) {
-            pq.add(Integer.parseInt(br.readLine()));
+            numbers[i] = Integer.parseInt(br.readLine());
         }
 
+        int[] sortedNumbers = selectionSort(numbers);
         StringBuilder answer = new StringBuilder();
-        while (!pq.isEmpty()) {
-            answer.append(pq.poll()).append("\n");
+        for (int i = 0; i < N; i++) {
+            answer.append(sortedNumbers[i]).append("\n");
         }
 
         System.out.println(answer);
     }
+
+    public static int[] selectionSort(int[] numbers) {
+        int N = numbers.length;
+        for (int i = 0; i < N - 1; i++) {
+            int min = numbers[i];
+            for (int j = i + 1; j < N; j++) {
+                if (numbers[j] < min) {
+                    min = numbers[j];
+                    numbers[j] = numbers[i];
+                    numbers[i] = min;
+                }
+            }
+        }
+
+        return numbers;
+    }
 }
+/*
+우선순위큐를 사용하면 삽입할 때 O(logN)이므로 N개의 자료를 삽입하면서
+O(NlogN), 출력할 때도 O(logN)이니 N 개의 자료를 출력하면서 O(NlogN)
+
+선택정렬은 O(N^2)
+ */

c6bd6c73c7ec0402598ade8016e3f4d89c468760
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2024-01-26 19:05:37 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 208 ms, Memory: 17664 KB -BaekjoonHub

--- 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -1,10 +1,23 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int num = scanner.nextInt();
+
         System.out.println(1);
+
         System.out.println(0);
+
+        
+
     }
+
 }
+
+
+

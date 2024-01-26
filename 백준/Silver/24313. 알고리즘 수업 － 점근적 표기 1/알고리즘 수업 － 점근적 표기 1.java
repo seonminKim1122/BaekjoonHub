e59a392dd00a41d7b10f559dd5049f1221b66df0
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2024-01-26 19:06:26 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.java
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.java
@@ -1,23 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int a1 = Integer.parseInt(st.nextToken());
-        int a0 = Integer.parseInt(st.nextToken());
 
-        int c = Integer.parseInt(br.readLine());
-        int n0 = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int c = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int n0 = Integer.parseInt(st.nextToken());
 
-        if ((c - a1 >= 0) && (c - a1) * n0 >= a0) {
-            System.out.println(1);
-        } else {
-            System.out.println(0);
-        }
+            if (T + (N) * Math.abs(n0) <= Math.abs(c) * Math.abs(n0)) {
+                if (N<=c) {
+                    System.out.println(1);
+                }
+                else System.out.println(0);
+            } else System.out.println(0);
+
+
+
+
+
     }
 }

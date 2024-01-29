9980305a9bdc2b7787bef428bf0f3cc2aec38483
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2024-01-29 17:34:09 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 820 ms, Memory: 120764 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -11,35 +11,48 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[][] numbers = new int[N+1][N+1];
-        for (int i = 1; i <= N; i++) {
+        int[][] arr = new int[N][N];
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
-                numbers[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int[][] dp = new int[N + 1][N + 1];
+        int[][] S = new int[N + 1][N + 1];
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + numbers[i][j];
+                S[i][j] = S[i - 1][j] + S[i][j - 1] - S[i - 1][j - 1] + arr[i - 1][j - 1];
             }
         }
 
-        StringBuilder sb = new StringBuilder();
+        StringBuilder answer = new StringBuilder();
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
 
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
-
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
 
-            int result = dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1];
-            sb.append(result).append("\n");
+            answer.append(getPrefixSum(x1, y1, x2, y2, S)).append('\n');
         }
 
-        System.out.println(sb);
+        System.out.println(answer);
+    }
+
+    static int getPrefixSum(int x1, int y1, int x2, int y2, int[][] S) {
+        return S[x2][y2] - S[x1 - 1][y2] - S[x2][y1 - 1] + S[x1 - 1][y1 - 1];
     }
 }
+/*
+의사 코드
+1. N * M 배열을 입력 받는다.
+2. 누적합 배열 S 를 만든다.
+3. 누적합 배열을 활용해 구간합을 구한다.
+
+시간복잡도
+-> O(N * N + M)
+-> 누적합 배열 만드는 데에 O(N * N)
+-> M 개의 구간합 구하는 데에 O(M)
+ */

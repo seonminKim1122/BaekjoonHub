a414de0fa73588ae3d2b4ef5c89ea64b5de0e4b7
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2024-01-31 13:32:20 +0900

    [Silver I] Title: 미로 탐색, Time: 132 ms, Memory: 14664 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,64 +2,83 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.StringTokenizer;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
+
+    static int[][] graph;
+
+    static int N;
+    static int M;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        int[][] maze = new int[N][M];
+        graph = new int[N][M];
         for (int i = 0; i < N; i++) {
-            String[] input = br.readLine().split("");
+            String input = br.readLine();
             for (int j = 0; j < M; j++) {
-                maze[i][j] = Integer.parseInt(input[j]);
+                graph[i][j] = input.charAt(j) - '0';
             }
         }
 
+        System.out.println(bfs(new Node(0, 0, 1)));
+    }
+
+    static int bfs(Node node) {
+        Queue<Node> queue = new LinkedList<>();
         boolean[][] visited = new boolean[N][M];
-        Queue<int[]> queue = new LinkedList<>();
+        visited[node.x][node.y] = true;
+        queue.add(node);
 
-        queue.add(new int[]{0, 0, 1});
-        visited[0][0] = true;
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
 
-        int result = 1;
         while (!queue.isEmpty()) {
-            int[] point = queue.poll();
-            int i = point[0];
-            int j = point[1];
-            int block = point[2];
+            Node now = queue.poll();
 
-            if (i == N-1 && j == M-1) {
-                result = block;
-            }
+            for (int k = 0; k < 4; k++) {
+                int nx = now.x + dx[k];
+                int ny = now.y + dy[k];
 
-            // 상, 하, 좌, 우
-            if (i-1 >= 0 && !visited[i-1][j] && maze[i-1][j] != 0) {
-                queue.add(new int[]{i-1, j, block+1});
-                visited[i-1][j] = true;
-            }
+                if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+                if (visited[nx][ny] || graph[nx][ny] == 0) continue;
 
-            if (i+1 < N && !visited[i+1][j] && maze[i+1][j] != 0) {
-                queue.add(new int[]{i+1, j, block+1});
-                visited[i+1][j]= true;
-            }
+                if (nx == N - 1 && ny == M - 1) {
+                    return now.path + 1;
+                }
 
-            if (j-1 >= 0 && !visited[i][j-1] && maze[i][j-1] != 0) {
-                queue.add(new int[]{i, j-1, block+1});
-                visited[i][j-1] = true;
-            }
-
-            if (j+1 < M && !visited[i][j+1] && maze[i][j+1] != 0) {
-                queue.add(new int[]{i, j+1, block+1});
-                visited[i][j+1] = true;
+                queue.add(new Node(nx, ny, now.path + 1));
+                visited[nx][ny] = true;
             }
         }
 
-        System.out.println(result);
+        return -1;
+    }
+
+    static class Node {
+        int x;
+        int y;
+        int path;
+        Node (int i, int j, int path) {
+            this.x = i;
+            this.y = j;
+            this.path = path;
+        }
     }
 }
+/*
+의사코드
+1. (0, 0) 에서 BFS 시작
+2. 특정 위치에 도착했을 때 지나온 칸의 수 = path 에 저장
+3. 위치가 (N - 1, M - 1) 에 도달하면 지금까지 지나온 칸의 수 + 1 리턴
+
+시간복잡도
+-> BFS 탐색을 결국 이동 가능한 경로를 다 탐색하되 너비를 우선으로 하는 방식일 뿐이므로
+-> O(N * M)
+ */

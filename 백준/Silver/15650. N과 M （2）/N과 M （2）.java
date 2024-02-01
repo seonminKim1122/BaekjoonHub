e2be7b1d8e94131cf718cc2c5123b1d68207237f
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2024-02-02 08:49:54 +0900

    [Silver III] Title: N과 M (2), Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -8,6 +8,9 @@ public class Main {
     static int N;
     static int M;
 
+    static int[] path;
+    static StringBuilder answer = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -15,33 +18,34 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        boolean[] visited = new boolean[N + 1];
-        for (int i = 1; i <= N; i++) {
-            visited[i] = true;
-            dfs(i, visited, 1);
-            visited[i] = false;
-        }
+        path = new int[M];
+
+        permutation(0, 1);
+
+        System.out.println(answer);
     }
 
-    public static void dfs(int i, boolean[] visited, int depth) {
+    static void permutation(int depth, int start) {
         if (depth == M) {
-            print(visited);
+            for (int i = 0; i < M; i++) {
+                answer.append(path[i]).append(' ');
+            }
+            answer.append('\n');
             return;
         }
 
-        for (int j = i + 1; j <= N; j++) {
-            visited[j] = true;
-            dfs(j, visited, depth + 1);
-            visited[j] = false;
+        for (int i = start; i <= N; i++) {
+            path[depth] = i;
+            permutation(depth + 1, i + 1);
+            path[depth] = 0;
         }
-    }
-
-    public static void print(boolean[] visited) {
-        for (int i = 1; i <= N; i++) {
-            if (visited[i]) {
-                System.out.print(i + " ");
-            }
-        }
-        System.out.println();
     }
 }
+
+/*
+의사코드
+1. 중복되는 수열을 출력하면 안되므로 1번 노드부터 탐색을 시작했다면 2번을 첫 노드로 할 때는 1번을 탐색하지 않도록 한다.
+
+시간복잡도
+-> O(N!)
+ */

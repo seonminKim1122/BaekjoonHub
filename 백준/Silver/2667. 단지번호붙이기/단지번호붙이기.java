818fe76e98f1d907e086ce8c5f535733f7083441
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2024-02-01 10:13:12 +0900

    [Silver I] Title: 단지번호붙이기, Time: 128 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -6,57 +6,70 @@ import java.util.Collections;
 import java.util.List;
 
 public class Main {
-
-    static int houses = 0;
-
+    
+    static int[][] graph;
+    static int house = 0;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int N;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        int[][] map = new int[N][N];
+        N = Integer.parseInt(br.readLine());
+        graph = new int[N][N];
         for (int i = 0; i < N; i++) {
-            String[] input = br.readLine().split("");
+            String line = br.readLine();
             for (int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(input[j]);
+                graph[i][j] = line.charAt(j) - '0';
             }
         }
 
-        boolean[][] visited = new boolean[N][N];
-        List<Integer> results = new ArrayList<>();
+        int cnt = 0;
+        List<Integer> houses = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (map[i][j] == 1 && !visited[i][j]) {
-                    dfs(i, j, map, visited);
-                    results.add(houses);
-                    houses = 0;
+                if (graph[i][j] == 1) {
+                    house = 0;
+                    cnt++;
+                    dfs(i, j);
+                    houses.add(house);
                 }
             }
         }
 
-        Collections.sort(results);
-        StringBuilder sb = new StringBuilder();
-        sb.append(results.size()).append("\n");
-        for (int result : results) {
-            sb.append(result).append("\n");
+        Collections.sort(houses);
+
+        StringBuilder answer = new StringBuilder();
+        answer.append(cnt).append('\n');
+        for (int i = 0; i < houses.size(); i++) {
+            answer.append(houses.get(i)).append('\n');
         }
 
-        System.out.println(sb);
+        System.out.println(answer);
     }
 
-    public static void dfs(int i, int j, int[][] map, boolean[][] visited) {
-        visited[i][j] = true;
-
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
+    static void dfs(int i, int j) {
+        graph[i][j] = 0;
+        house++;
 
         for (int k = 0; k < 4; k++) {
-            int x = i + dx[k];
-            int y = j + dy[k];
+            int nx = i + dx[k];
+            int ny = j + dy[k];
 
-            if (x < 0 || y < 0 || x >= map.length || y >= map.length || visited[x][y] || map[x][y] == 0) continue;
+            if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+            if (graph[nx][ny] == 0) continue;
 
-            dfs(x, y, map, visited);
+            dfs(nx, ny);
         }
-        houses++;
     }
 }
+/*
+의사코드
+1. 집이 있는 곳에서 dfs() 수행해서 같은 단지의 집을 모두 탐색
+2. 단지 내 집 탐색 때마다 카운팅 하여 단지 내 집의 수 파악
+3. 전체 배열에 대해 해당 과정을 반복하고 dfs() 가 몇 번 수행됐는지를 통해 단지 수 파악
+
+시간복잡도
+O(N^2)
+*/

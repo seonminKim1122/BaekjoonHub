27f20bc2204d8f9b9bd7d36d2496c2f3e0778e2f
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2024-02-02 08:42:02 +0900

    [Silver III] Title: N과 M (1), Time: 268 ms, Memory: 22192 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -5,28 +5,29 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static StringBuilder answer = new StringBuilder();
     static int[] path;
     static boolean[] via;
+    static StringBuilder answer = new StringBuilder();
 
     static int N;
     static int M;
 
-    public static void main(String[] args)  throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+
         path = new int[M];
         via = new boolean[N + 1];
 
-        permutation(0);
+        backTracking(0);
 
         System.out.println(answer);
     }
 
-    static void permutation(int depth) {
+    static void backTracking(int depth) {
         if (depth == M) {
             for (int i = 0; i < M; i++) {
                 answer.append(path[i]).append(' ');
@@ -36,13 +37,20 @@ public class Main {
         }
 
         for (int i = 1; i <= N; i++) {
-            if (!via[i]) {
-                path[depth] = i;
-                via[i] = true;
-                permutation(depth + 1);
-                via[i] = false;
-                path[depth] = 0;
-            }
+            if (via[i]) continue;
+            via[i] = true;
+            path[depth] = i;
+            backTracking(depth + 1);
+            via[i] = false;
+            path[depth] = 0;
         }
     }
 }
+/*
+의사코드
+1. 백트래킹 방식을 이용해 탐색한 depth == M 이 되는 경우 더 탐색을 하지 않고 현재까지 지나온 노드를 저장한 후
+2. 다시 이전 노드로 돌아가는 방식을 통해 문제에서 요구하는 값을 구한다.
+
+시간복잡도
+-> O(N!/(N-M)!)
+ */

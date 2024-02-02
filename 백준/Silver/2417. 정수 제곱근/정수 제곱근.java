47561a911ef1feca8720ababf940864b99ff682b
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2024-02-02 23:02:16 +0900

    [Silver IV] Title: 정수 제곱근, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/2417. 정수 제곱근/정수 제곱근.java
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.java
@@ -3,16 +3,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         long n = Long.parseLong(br.readLine());
-        long sqrt = (long) Math.sqrt(n);
 
-        if (sqrt * sqrt >= n) {
-            System.out.println(sqrt);
-        } else {
-            System.out.println(sqrt + 1);
+        long start = 0;
+        long end = 2_147_483_648L * 2L; // 2^32
+
+        while (start <= end) {
+            long mid = (start + end) / 2;
+            double v = (double)mid * mid;
+            if (v < n) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
         }
+
+        System.out.println(start);
     }
 }

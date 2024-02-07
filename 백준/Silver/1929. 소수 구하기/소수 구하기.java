de79f79d9f83aae0f061e416812159204741dffd
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2024-02-07 10:08:54 +0900

    [Silver III] Title: 소수 구하기, Time: 284 ms, Memory: 20236 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,33 +1,32 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int M = sc.nextInt();
-        int N = sc.nextInt();
 
-        for (int i = M; i <= N; i++) {
-            if(isPrime(i)) {
-                System.out.println(i);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        boolean[] isNotPrime = new boolean[end + 1];
+        isNotPrime[1] = true;
+        for (int i = 2; i <= end; i++) {
+            for (int j = i * 2; j <= end; j += i) {
+                isNotPrime[j] = true;
             }
         }
-    }
 
-    public static boolean isPrime(int num) {
-        // 예외 처리
-        if (num == 1) {
-            return false;
-        }
-
-        if (num == 2) {
-            return true;
-        }
-        
-        // 소수인지 판별
-        for(int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) {
-                return false;
+        StringBuilder answer = new StringBuilder();
+        for (int i = start; i <= end; i++) {
+            if (!isNotPrime[i]) {
+                answer.append(i).append('\n');
             }
         }
-        return true;
+
+        System.out.println(answer);
     }
 }

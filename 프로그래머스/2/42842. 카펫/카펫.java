8b6bc7559b9367fe4c13e16787f1880250c958fe
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2024-02-10 20:19:41 +0900

    [level 2] Title: 카펫, Time: 0.14 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        
+        int a = brown / 2;
+        int b = yellow;
+        
+        int temp = (int)Math.sqrt(a * (a - 4) - 4 * (b - 1));
+        System.out.println(temp);
+        int N = (a + 2 - temp) / 2;
+        int M = a + 2 - N;
+        
+        int[] answer = {M, N};
+        
+        return answer;
+    }
+}

a7185e4d5b3903de007b367bf1fa62a42e555163
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2024-02-12 18:04:40 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1656 ms, Memory: 336480 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -5,59 +5,90 @@ import java.util.*;
 
 public class Main {
 
+    static int INF = 987654321;
+    static int[] dist;
+    static List<List<Edge>> graph = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken()); // 도시의 개수
-        int M = Integer.parseInt(st.nextToken()); // 도로의 개수
-        int K = Integer.parseInt(st.nextToken()); // 거리 정보
-        int X = Integer.parseInt(st.nextToken()) - 1; // 출발 도시 번호
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
 
-        List<Integer>[] roads = new List[N];
-        for (int i = 0; i < N; i++) {
-            roads[i] = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
         }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
 
-            int from = Integer.parseInt(st.nextToken()) - 1;
-            int to = Integer.parseInt(st.nextToken()) - 1;
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
 
-            roads[from].add(to);
+            graph.get(from).add(new Edge(to, 1));
         }
 
-        System.out.println(dijkstra(X, roads, K));
-    }
+        dist = new int[N + 1];
+        dijkstra(X);
 
-    public static StringBuilder dijkstra(int X, List<Integer>[] roads, int K) {
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
-        priorityQueue.add(X);
-
-        int N = roads.length;
-        int[] dist = new int[N];
-        Arrays.fill(dist, N);
-        dist[X] = 0;
-
-        while (!priorityQueue.isEmpty()) {
-            int from = priorityQueue.poll();
-
-            for (int to : roads[from]) {
-                if (dist[to] > dist[from] + 1) {
-                    dist[to] = dist[from] + 1;
-                    priorityQueue.add(to);
-                }
+        StringBuilder answer = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            if (dist[i] == K) {
+                answer.append(i).append('\n');
             }
         }
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            if (dist[i] == K) sb.append(i + 1).append("\n");
-        }
-        sb.setLength(Math.max(sb.length() - 1, 0));
-        if (sb.length() == 0) sb.append(-1);
+        System.out.println(answer.length() == 0 ? -1 : answer);
+    }
 
-        return sb;
+    static void dijkstra(int start) {
+        Arrays.fill(dist, INF);
+        dist[start] = 0;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+        pq.add(new int[]{start, dist[start]});
+
+        while (!pq.isEmpty()) {
+            int[] now = pq.poll();
+
+            int from = now[0];
+
+            List<Edge> edges = graph.get(from);
+
+            for (Edge edge : edges) {
+                int to = edge.to;
+                int value = edge.dist;
+
+                if (dist[to] > dist[from] + value) {
+                    dist[to] = dist[from] + value;
+                    pq.add(new int[]{to, dist[to]});
+                }
+            }
+        }
+    }
+
+    static class Edge {
+        int to;
+        int dist;
+
+        Edge(int to, int dist) {
+            this.to = to;
+            this.dist = dist;
+        }
     }
 }
+
+/*
+의사코드
+1. X 를 출발지로 설정해서 다익스트라 알고리즘을 적용한다
+-> 문제에서 모든 간선의 길이는 1이라고 했으므로 간선이 양수임이 보장된다.
+2. 다익스트라 알고리즘을 통해 얻은 dist 배열에서 값이 K 인 노드 번호를 찾아 오름차순으로
+한 줄에 하나씩 출력한다
+
+시간복잡도
+O(NlogN)
+-> 우선순위 큐를 사용하면 최단 거리 노드를 찾는데 logN 의 시간이 소요
+ */

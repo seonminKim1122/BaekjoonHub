f6c43dee9b54bb22834e18a2e9ac7fe45c5f2405
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2024-03-19 17:45:50 +0900

    [Gold III] Title: 양팔저울, Time: 128 ms, Memory: 14432 KB -BaekjoonHub

--- 백준/Gold/2629. 양팔저울/양팔저울.java
+++ 백준/Gold/2629. 양팔저울/양팔저울.java
@@ -4,49 +4,47 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-
-        int[] weights = new int[N + 1];
-        int V = 0;
+        // 추 정보
+        int N = Integer.parseInt(br.readLine()); // 추의 개수
+        int[] weights = new int[N];
+        int totalWeight = 0;
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i < N; i++) {
             weights[i] = Integer.parseInt(st.nextToken());
-            V += weights[i];
+            totalWeight += weights[i];
+        }
+        // 추를 이용해 측정 되는지 확인하고자 하는 타겟 무게 정보
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        int[] targets = new int[M];
+        for (int i = 0; i < M; i++) {
+            targets[i] = Integer.parseInt(st.nextToken());
         }
 
-        boolean[][] dp = new boolean[N + 1][V + 1];
+        // 로직
+        boolean[][] dp = new boolean[N + 1][totalWeight + 1];
         dp[0][0] = true;
 
         for (int i = 1; i <= N; i++) {
-            int weight = weights[i];
-
-            for (int j = 0; j <= V - weight; j++) {
+            int weight = weights[i - 1];
+            for (int j = 0; j <= totalWeight; j++) {
                 if (dp[i - 1][j]) {
-                    dp[i][j] = dp[i - 1][j];
+                    dp[i][j] = true;
                     dp[i][j + weight] = true;
                     dp[i][Math.abs(j - weight)] = true;
                 }
             }
         }
 
-        int M = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < M; i++) {
-            int target = Integer.parseInt(st.nextToken());
-
-            if (target <= V && dp[N][target]) {
-                result.append("Y");
+        for (int target : targets) {
+            if (target > totalWeight) {
+                System.out.print("N ");
             } else {
-                result.append("N");
+                System.out.print(dp[N][target] ? "Y " : "N ");
             }
-            result.append(" ");
         }
-
-        System.out.println(result);
     }
 }

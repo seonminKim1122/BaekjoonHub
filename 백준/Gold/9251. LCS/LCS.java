cc0864ff09353bd95cbfb3501b689c16ab61cc93
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2024-03-22 09:21:18 +0900

    [Gold V] Title: LCS, Time: 144 ms, Memory: 18276 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -3,26 +3,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        char[] s1 = br.readLine().toCharArray();
-        char[] s2 = br.readLine().toCharArray();
+        char[] word1 = br.readLine().toCharArray();
+        char[] word2 = br.readLine().toCharArray();
 
-        int R = s1.length;
-        int C = s2.length;
+        int N = word1.length;
+        int M = word2.length;
+        int[][] dp = new int[N + 1][M + 1];
 
-        int[][] dp = new int[R + 1][C + 1];
-        for (int i = 1; i <= R; i++) {
-            for (int j = 1; j <= C; j++) {
-                if (s1[i - 1] == s2[j - 1]) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                if (word1[i - 1] == word2[j - 1]) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-
-        System.out.println(dp[R][C]);
+        System.out.println(dp[N][M]);
     }
 }

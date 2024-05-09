e525e98362f549a55e3ac2a9e7feac59398a425a
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2024-05-09 14:56:40 +0900

    [level 2] Title: 의상, Time: 0.07 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,15 +2,14 @@ import java.util.*;
 
 class Solution {
     
-    static int answer = -1;
     public int solution(String[][] clothes) {
-
+        int answer = 1;
         Map<String, Integer> map = new HashMap<>();
         
         int N = clothes.length;
         for (int i = 0; i < N; i++) {
             String category = clothes[i][1];
-            map.put(category, map.getOrDefault(category, 0) + 1);
+            map.put(category, map.getOrDefault(category, 1) + 1);
         }
             
         List<String> categories = new ArrayList<>();
@@ -18,23 +17,33 @@ class Solution {
             categories.add(category);
         }
         
-        N = categories.size();
-        solve(0, N, categories, 0, 1, map);
-        
-        return answer;
-    }
-    
-    void solve(int depth, int N, List<String> categories, int start, int temp, Map<String, Integer> map) {
-        answer += temp;
-        if (depth == N) return;
-        
-        for (int i = start; i < N; i++) {
-            String category = categories.get(i);
-            solve(depth + 1, N, categories, i + 1, temp * map.get(category), map);
+        for (String category : categories) {
+            answer *= map.get(category);
         }
+        
+        
+        return answer - 1;
     }
 }
 
 /*
-해볼게
+
+2 1 3
+
+2C1 + 1C1 + 3C1
+
++ 2C1 * 1C1
++ 2C1 * 3C1
++ 1C1 * 3C1
+
++ 2C1 * 1C1 * 3C1
+
+headgear : 2
+eyewear: 1
+
+2, 1, 2 -> 5
+
+(2 1)
+
+6 - (2 + 1) * (1 + 1) - 1 
 */

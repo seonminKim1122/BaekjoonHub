f8334bce45d90da543693c4246aae784337a3f55
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2024-07-14 13:20:37 +0900

    [level 2] Title: 시소 짝꿍, Time: 119.97 ms, Memory: 97.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int[] weights) {
+        long answer = 0;
+        
+        Arrays.sort(weights);
+        Map<Double, Integer> map = new HashMap<>();
+        for (int weight : weights) {
+            double a = weight * 1.0;
+            double b = weight * 3.0 / 4.0;
+            double c = weight * 2.0 / 3.0;
+            double d = weight / 2.0;
+            
+            if (map.containsKey(a)) answer += map.get(a);
+            if (map.containsKey(b)) answer += map.get(b);
+            if (map.containsKey(c)) answer += map.get(c);
+            if (map.containsKey(d)) answer += map.get(d);
+            
+            map.put(a, map.getOrDefault(a, 0) + 1);
+        }
+        
+        return answer;
+    }
+}
+
+/*
+1, 1.333, 1.5, 2
+-> 4가지 케이스 
+*/

96ec80c9fc7e2e6bfed3101bdcd94989d57b88c2
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2024-10-31 13:27:19 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 2.06 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -1,52 +1,39 @@
-import java.util.*;
-
 class Solution {
-    
     public int solution(String s) {
         StringBuilder sb = new StringBuilder();
         sb.append('#');
         for (char c : s.toCharArray()) {
-            sb.append(c).append('#');
+                sb.append(c).append('#');
         }
-        
+
         String str = sb.toString();
         int N = str.length();
-        char[] chars = str.toCharArray();
-        int[] P = new int[N];
+
+        char[] alpha = str.toCharArray();
+        int[] P = new int[N]; // P[i] : i 를 중심으로 한 팰린드롬의 반지름
+
         int C = 0;
         int R = 0;
-        
-        int answer = 1;
+        int answer = 0;
         for (int i = 0; i < N; i++) {
+            // 재활용 가능한 부분은 재활용
             if (i <= R) {
                 P[i] = Math.min(P[2 * C - i], R - i);
-            } 
+            }
             
+            // 재활용 한 부분 다음부터 팰린드롬 계산
             while (i - P[i] - 1 >= 0 && i + P[i] + 1 < N) {
-                if (chars[i - P[i] - 1] != chars[i + P[i] + 1]) break;
-                P[i]++;
+                    if (alpha[i - P[i] - 1] != alpha[i + P[i] + 1]) break;
+                    P[i]++;
             }
-            
-            if (R < i + P[i]) {
-                C = i;
-                R = i + P[i];
+
+            if (i + P[i] > R) {
+                    C = i;
+                    R = i + P[i];
             }
-            
             answer = Math.max(answer, P[i]);
         }
-        
+
         return answer;
     }
 }
-
-/*
-dp[i] => i번째 문자를 끝으로 하는 가장 긴 팰린드롬의 길이
-
-dp[i - 1] => 홀수 팰린드롬일 때
-1. 홀수 to 홀수
-alpha[i - (dp[i - 1] + 1)] == alpha[i]
-2. 홀수 to 짝수
-
-
-dp[i - 1] => 짝수 팰린드롬일 때
-*/

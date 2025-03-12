642675bfe44d20c2520a970a2b56b083a35dae27
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-03-12 21:30:54 +0900

    [Bronze V] Title: 검증수, Time: 104 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.java
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -4,15 +4,19 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int result = 0;
+        int sum = 0;
         for (int i = 0; i < 5; i++) {
             int num = Integer.parseInt(st.nextToken());
-            result += (num * num);
+            sum += (num * num) % 10;
         }
-        System.out.println(result % 10);
+
+        System.out.println(sum % 10);
     }
+
+
 }

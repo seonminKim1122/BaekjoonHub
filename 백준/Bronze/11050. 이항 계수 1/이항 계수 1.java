7635d551ba2e353ed0b6cf81e1780cabe00e40ca
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-03-12 21:55:43 +0900

    [Bronze I] Title: 이항 계수 1, Time: 108 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -1,22 +1,24 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        int result = 1;
-
-        for(int i = N; i > K; i--) {
-            result *= i;
+        int numerator = 1;
+        int denominator = 1;
+        for (int i = 0; i < K; i++) {
+            numerator *= (N - i);
+            denominator *= (K - i);
         }
 
-        for(int i = N-K; i > 0; i--) {
-            result /= i;
-        }
-        System.out.println(result);
+        System.out.println(numerator / denominator);
     }
 }

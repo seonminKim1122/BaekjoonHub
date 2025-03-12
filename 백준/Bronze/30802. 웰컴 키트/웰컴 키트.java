1f3f54d8663c390c525bfc0e9fb7e0362ebced6e
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-03-12 21:52:24 +0900

    [Bronze III] Title: 웰컴 키트, Time: 120 ms, Memory: 16004 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
@@ -1,33 +1,34 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         int N = Integer.parseInt(br.readLine());
-        
+
+
+        int[] applicants = new int[6];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] shirts = new int[6];
         for (int i = 0; i < 6; i++) {
-            shirts[i]= Integer.parseInt(st.nextToken()); 
+            applicants[i] = Integer.parseInt(st.nextToken());
         }
-        
+
         st = new StringTokenizer(br.readLine());
         int T = Integer.parseInt(st.nextToken());
         int P = Integer.parseInt(st.nextToken());
-        
-        // 티셔츠 T장씩 최소 몇 묶음 주문해야하는지 계산
-        int ans1 = 0;
+
+        // 티셔츠: applicants[i] - 1 / T + 1
+        long tShirts = 0;
         for (int i = 0; i < 6; i++) {
-            if (shirts[i] == 0) continue;
-            ans1 += (shirts[i] - 1) / T + 1;
+            tShirts += applicants[i] / T + (applicants[i] % T > 0 ? 1 : 0);
         }
-        
-        System.out.println(ans1);
-        
-        // 펜을 P자루씩 최대 몇 묶음 주문할 수 있는지, 그 때 펜 한자루 몇 개 주문하는지
+
+        System.out.println(tShirts);
+        // 펜: N / P 묶음, N % P 개 주문
         System.out.println(N / P + " " + N % P);
     }
 }

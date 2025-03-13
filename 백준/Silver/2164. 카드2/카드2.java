fdcedb913a10c71196e0eb9500d6efc9729b4969
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-03-13 21:27:24 +0900

    [Silver IV] Title: 카드2, Time: 156 ms, Memory: 45448 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -5,23 +5,27 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-
-        Queue<Integer> queue = new LinkedList<>();
+        Queue<Integer> deck = new LinkedList<>();
         for (int i = 1; i <= N; i++) {
-            queue.add(i);
+            deck.add(i);
         }
 
-        while (queue.size() > 1) {
-            // 제일 위의 카드 버리기
-            queue.poll();
-            // 그 다음, 제일 위에 있는 카드를 제일 아래에 있는 카드 밑으로 옮기기
-            queue.add(queue.poll());
+        while (N > 1) {
+            // 맨 윗장 버리기
+            deck.poll();
+            // 현재 맨 윗장을 맨 아래로 옮기기
+            deck.add(deck.poll());
+            N--;
         }
 
-        System.out.println(queue.peek());
+        System.out.println(deck.poll());
     }
+
+
+
 }

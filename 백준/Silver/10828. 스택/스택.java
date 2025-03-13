5d39cd8a958757b088583e61eb1aaff0d83a57ec
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-03-13 22:02:23 +0900

    [Silver IV] Title: 스택, Time: 172 ms, Memory: 20676 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,50 +1,79 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
+import java.io.*;
+import java.util.Arrays;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        Stack<Integer> stack = new Stack<>();
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
+        Stack stack = new Stack(N);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        while (N > 0) {
             String[] order = br.readLine().split(" ");
+
             switch (order[0]) {
                 case "push":
                     stack.push(Integer.parseInt(order[1]));
                     break;
                 case "pop":
-                    if (stack.isEmpty()) {
-                        sb.append(-1);
-                    } else {
-                        sb.append(stack.pop());
-                    }
-                    sb.append("\n");
+                    bw.write(String.valueOf(stack.pop()));
+                    bw.write("\n");
                     break;
                 case "size":
-                    sb.append(stack.size()).append("\n");
+                    bw.write(String.valueOf(stack.size()));
+                    bw.write("\n");
                     break;
                 case "empty":
-                    if (stack.isEmpty()) {
-                        sb.append(1);
-                    } else {
-                        sb.append(0);
-                    }
-                    sb.append("\n");
+                    bw.write(String.valueOf(stack.empty()));
+                    bw.write("\n");
                     break;
                 default:
-                    if (stack.isEmpty()) {
-                        sb.append(-1);
-                    } else {
-                        sb.append(stack.peek());
-                    }
-                    sb.append("\n");
+                    bw.write(String.valueOf(stack.top()));
+                    bw.write("\n");
             }
+
+            N--;
         }
 
-        System.out.println(sb);
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+}
+
+class Stack {
+
+    int[] arr;
+    int top = -1;
+
+    Stack(int N) {
+        arr = new int[N];
+    }
+
+    void push(int X) {
+        arr[++top] = X;
+    }
+
+    int pop() {
+        if (size() == 0) return -1;
+
+        return arr[top--];
+    }
+
+    int size() {
+        return top + 1;
+    }
+
+    int empty() {
+        return size() == 0 ? 1 : 0;
+    }
+
+    int top() {
+        if (size() == 0) return -1;
+
+        return arr[top];
     }
 }

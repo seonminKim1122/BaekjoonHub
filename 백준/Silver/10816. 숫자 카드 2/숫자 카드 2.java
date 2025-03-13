5b2ed230663662a1bf9433624de5121f370021aa
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-03-13 21:39:25 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1052 ms, Memory: 169760 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,30 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Map<Integer, Integer> countMap = new HashMap<>();
-
+        StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        Map<Integer, Integer> countMap = new HashMap<>();
         for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            countMap.put(num, countMap.getOrDefault(num, 0)+1);
+            int number = Integer.parseInt(st.nextToken());
+            countMap.put(number, countMap.getOrDefault(number, 0) + 1);
         }
 
         int M = Integer.parseInt(br.readLine());
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         for (int i = 0; i < M; i++) {
-            sb.append(countMap.getOrDefault(Integer.parseInt(st2.nextToken()), 0)).append(" ");
+            int number = Integer.parseInt(st.nextToken());
+            if (i != 0) bw.write(" ");
+            bw.write(String.valueOf(countMap.getOrDefault(number, 0)));
         }
 
-        System.out.println(sb);
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }

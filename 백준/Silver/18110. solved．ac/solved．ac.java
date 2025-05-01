74688df02d806104ccfd6be42f4aa48ad3cff778
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-05-01 21:38:44 +0900

    [Silver IV] Title: solved.ac, Time: 312 ms, Memory: 37516 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.java
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -1,52 +1,48 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        
         int n = Integer.parseInt(br.readLine());
-
-        // 아직 아무 의견이 없으면 0
+        
         if (n == 0) {
             System.out.println(0);
             return;
         }
-
+        
         int[] scores = new int[n];
         for (int i = 0; i < n; i++) {
             scores[i] = Integer.parseInt(br.readLine());
         }
-        int except = calcExceptCnt(n, 15);
-
-        System.out.println(solve(scores, except));
+        
+        System.out.println(solve(scores, countExcept(n, 15)));
     }
-
-    private static int calcExceptCnt(int n, int percent) {
-        return round(n * percent, 100);
-    }
-
+    
     private static int solve(int[] scores, int except) {
-        // 오름차순 정렬
+        // score 정렬
         Arrays.sort(scores);
-
-        // 위 아래에서 except 명씩 제외하고 평균 구하기
+        
+        // 절사 평균 구할 대상 수
         int cnt = scores.length - 2 * except;
-
-        int result = 0;
+        
+        int sum = 0;
         for (int i = except; i < except + cnt; i++) {
-            result += scores[i];
+            sum += scores[i];
         }
-
-        return round(result, cnt);
+        
+        return calc(sum, cnt);
     }
-
-    private static int round(int numerator, int denominator) {
+    
+    private static int countExcept(int n, int percent) {
+        return calc(n * percent, 100);
+    }
+    
+    private static int calc(int numerator, int denominator) {
         if (2 * (numerator % denominator) < denominator) {
-            return numerator / denominator;
+	          return numerator / denominator;
         } else {
             return numerator / denominator + 1;
         }

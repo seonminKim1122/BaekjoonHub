81b67c96856471f2bc8667ef77b216276c761ce4
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-05-02 22:45:30 +0900

    [Silver III] Title: 프린터 큐, Time: 136 ms, Memory: 16132 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -3,61 +3,79 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int T = Integer.parseInt(br.readLine());
-
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < T; i++) {
-            String[] input = br.readLine().split(" ");
+        int T = Integer.parseInt(br.readLine());
+        while (T > 0) {
+            T--;
 
-            int N = Integer.parseInt(input[0]);
-            int M = Integer.parseInt(input[1]);
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-            Queue<Integer> queue = new LinkedList<>();
-            String[] importances = br.readLine().split(" ");
-            for (String importance : importances) {
-                queue.add(Integer.parseInt(importance));
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            st = new StringTokenizer(br.readLine());
+
+            Queue<Document> queue = new LinkedList<>();
+            for (int i = 0; i < N; i++) {
+                int importance = Integer.parseInt(st.nextToken());
+                queue.add(new Document(i, importance));
             }
 
 
-            int targetIdx = M;
+            int sequence = 0;
             int result = 0;
+            while (!queue.isEmpty()) {
+                Document front = queue.poll();
 
-            int max = getMax(queue);
-            while (true) {
-                if (queue.peek() != max) {
-                    queue.add(queue.poll());
+                if (isBiggerExist(queue, front)) {
+                    queue.add(front);
                 } else {
-                    queue.poll();
-                    result++;
-                    if (targetIdx == 0) {
-                        sb.append(result).append("\n");
+                    sequence++;
+                    if (front.startIdx == M) {
+                        result = sequence;
                         break;
                     }
-                    max = getMax(queue);
                 }
-                targetIdx = (targetIdx - 1) < 0 ? queue.size() - 1 : targetIdx - 1;
             }
+
+            sb.append(result);
+            sb.append('\n');
         }
+
 
         System.out.println(sb);
     }
 
-    public static int getMax(Queue<Integer> queue) {
-        int max = 0;
+    // queue 에서 doc 보다 더 중요도가 높은 문서가 존재하는지 확인하는 메서드
+    private static boolean isBiggerExist(Queue<Document> queue, Document doc) {
+        for (Document other: queue) {
+            if (doc.compareTo(other) > 0) return true;
+        }
+        return false;
+    }
 
-        for (int number : queue) {
-            if (max < number) {
-                max = number;
-            }
+
+    static class Document implements Comparable<Document> {
+
+        int startIdx = -1;
+        int importance = 0;
+
+        Document(int startIdx, int importance) {
+            this.startIdx = startIdx;
+            this.importance = importance;
         }
 
-        return max;
+        @Override
+        public int compareTo(Document other) {
+            return other.importance - this.importance;
+        }
     }
 }

24b6dc8768c1e8a46522865147a607f7070b1f4b
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-05-03 15:14:04 +0900

    [Bronze II] Title: 단어의 개수, Time: 300 ms, Memory: 51848 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,17 +1,25 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String words = br.readLine();
 
-        String S = br.readLine();
-        S = S.trim();
-
-        if (S.length() == 0) {
-            System.out.println(0);
-        } else {
-            System.out.println(S.split(" ").length);
+        int count = 0;
+        for (String word : words.split(" ")){
+            if(word.isEmpty()) {
+                continue;
+            }else{
+                count++;
+            }
         }
 
+        System.out.println(count);
+
     }
+
 }

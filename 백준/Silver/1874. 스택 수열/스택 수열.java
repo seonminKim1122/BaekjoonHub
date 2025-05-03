fedd476a380e5db7516d3b1e86534c184f1635f5
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-05-03 15:43:35 +0900

    [Silver II] Title: 스택 수열, Time: 328 ms, Memory: 25660 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -4,57 +4,47 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
+        int[] sequence = new int[n];
+        for (int i = 0;i < n; i++) {
+            sequence[i] = Integer.parseInt(br.readLine());
+        }
 
         Stack<Integer> stack = new Stack<>();
+        int number = 1;
+        int idx = 0;
+        /**
+         * stack.peek() < sequence[idx]
+         * => 찾고자 하는 숫자가 아직 stack에 쌓이지 않았으므로
+         * => stack.push(number++);
+         *
+         * stack.peek() == sequence[idx]
+         * => 찾고자 하는 숫자를 stack 에서 제거
+         *
+         * stack.peek() > sequence[idx]
+         * => 수열을 만들 수 없으므로 NO
+         */
+
         StringBuilder answer = new StringBuilder();
-
-        int num = 1;
-        for (int i = 0; i < n; i++) {
-            int need = Integer.parseInt(br.readLine());
-
-            if (num > need) { // 이미 필요한 값을 스택에 넣은 적이 있다
-                boolean flag = false;
-                while (!stack.isEmpty()) {
-                    int top = stack.pop();
-                    answer.append("-\n");
-                    if (top == need) {
-                        flag = true;
-                        break;
-                    }
-                }
-                
-                if (!flag) {
-                    answer.setLength(0);
-                    answer.append("NO");
-                    break;
-                }
-            } else { // 아직 넣지 않았다
-                while (num <= need) {
-                    stack.push(num++);
-                    answer.append("+\n");
-                }
+        while (idx < n) {
+            if (stack.isEmpty() || stack.peek() < sequence[idx]) {
+                stack.push(number++);
+                answer.append("+\n");
+            } else if (stack.peek() == sequence[idx]) {
                 stack.pop();
                 answer.append("-\n");
+                idx++;
+            } else {
+                answer.setLength(0);
+                answer.append("NO");
+                break;
             }
         }
 
         System.out.println(answer);
     }
 }
-/*
-필요한 값 : m
-현재 넣어야 하는 값 : n
-
-m > n 이면 n == m 될 때까지 push
-m < n 이면 stack에서 pop 하면서 해당 원소 찾기
-
-이미 다른 요소를 뽑는 과정에서 pop 이 되버렸으면 해당 원소를 찾을 수 없을 것이고
-이 경우 stack.isEmpty() -> true 가 될 때 까지 flag 값이 False
-
-시간복잡도 : O(N)
-이유 : for 문 내부에서는 O(1) 인 연산들이 여러 번 수행되는 것일 뿐임
- */

4cbd8f4ad37de0c21e5fe63e5dae0cf0ab407890
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-05-04 21:06:58 +0900

    [Silver II] Title: 랜선 자르기, Time: 152 ms, Memory: 17696 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -8,35 +8,47 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int K = Integer.parseInt(st.nextToken()); // 가지고 있는 랜선의 개수
-        int N = Integer.parseInt(st.nextToken()); // 필요한 랜선의 개수
+        // K, N
+        int K = Integer.parseInt(st.nextToken()); // 1 <= K <= 10,000
+        int N = Integer.parseInt(st.nextToken()); // 1 <= N <= 1,000,000
 
         int[] lans = new int[K];
-        long start = 1;
-        long end = 0;
         for (int i = 0; i < K; i++) {
             lans[i] = Integer.parseInt(br.readLine());
-            if (end < lans[i]) {
-                end = lans[i];
-            }
         }
 
-        // N 개를 만들 수 있는 랜선의 최대 길이를 구하라
+        System.out.println(solve(lans, N));
+    }
+
+    private static long solve(int[] lans, int N) {
+        // lans: 오영식이 보유하고 있는 K 개의 랜선
+        // N: 박성원이 캠프 때 쓸 랜선 수
+
+        long start = 1;
+        long end = Integer.MAX_VALUE; // 랜선의 길이는 2^31 - 1 보다 작거나 같은 자연수
+        long answer = -1;
         while (start <= end) {
             long mid = (start + end) / 2;
 
-            int cnt = 0;
-            for (int i = 0; i < K; i++) {
-                cnt += (lans[i] / mid);
-            }
-
-            if (cnt < N) {
+            long cnt = calc(lans, mid);
+            if (cnt >= N) {
+                answer = mid;
+                start = mid + 1;
+            } else {
                 end = mid - 1;
-            } else  {
-                start = mid + 1; // == N 도 포함시킨 이유는 == N이 되는 최대를 찾아야 하기 때문
             }
         }
 
-        System.out.println(end);
+        return answer;
+    }
+
+    private static long calc(int[] lans, long size) {
+        long result = 0;
+
+        for (int lan : lans) {
+            result += (lan / size);
+        }
+
+        return result;
     }
 }

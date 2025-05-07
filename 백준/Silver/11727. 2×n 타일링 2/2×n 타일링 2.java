4159547b4c44c72e92b04b1c6942753401fdea82
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-05-07 22:52:52 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 104 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -1,15 +1,27 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] dp = new int[n+1];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        /*
+        1 x 2, 2 x 1, 2 x 2 로 2 x n 만들기
+
+        => 2 x (n - 2) + (2 x 2)
+        => 2 x (n - 2) + 2 * (1 x 2)
+        => 2 x (n - 1) + (2 x 1)
+         */
+
+        int[] dp = new int[n + 1];
         dp[0] = 1;
         dp[1] = 1;
+
         for (int i = 2; i <= n; i++) {
-            dp[i] = ((dp[i-1] % 10007) + ((dp[i-2] * 2 ) % 10007)) % 10007;
+            dp[i] = (2 * dp[i - 2]) % 10007 + dp[i - 1] % 10007;
+            dp[i] = dp[i] % 10007;
         }
+
         System.out.println(dp[n]);
     }
 }

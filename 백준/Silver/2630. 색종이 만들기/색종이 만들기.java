be0d7a2eda028437a1a9a86fd9961175069adf26
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-05-08 22:48:51 +0900

    [Silver II] Title: 색종이 만들기, Time: 132 ms, Memory: 15600 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -1,54 +1,60 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    private static int white = 0;
-    private static int blue = 0;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
 
-        int[][] paper = new int[N][N];
+        int N = Integer.parseInt(br.readLine());
+        int[][] square = new int[N][N];
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                paper[i][j] = Integer.parseInt(st.nextToken());
+                square[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        checkAndCut(paper, 0, 0, N-1, N-1);
-        System.out.println(white);
-        System.out.println(blue);
+        int[] answer = solve(square, N);
+        System.out.println(answer[0]);
+        System.out.println(answer[1]);
     }
 
-    public static void checkAndCut(int[][] paper, int startX, int startY, int endX, int endY) {
-        int criteria = paper[startX][startY];
-        boolean isOkay = true;
+    private static int[] solve(int[][] square, int N) {
+        int[] result = new int[2];
+        recursive(square, 0, 0, N, N, result);
+        return result;
+    }
 
-        for (int i = startX; i <= endX; i++) {
-            for (int j = startY; j <= endY; j++) {
-                if (paper[i][j] != criteria) {
-                    isOkay = false;
-                    break;
-                }
-            }
-        }
+    private static void recursive(int[][] square, int startX, int startY, int endX, int endY, int[] result) {
 
-        if (!isOkay) {
-            checkAndCut(paper, startX, startY, (startX+endX)/2, (startY+endY)/2);
-            checkAndCut(paper, startX, (startY+endY)/2 + 1, (startX+endX)/2, endY);
-            checkAndCut(paper, (startX+endX)/2 + 1, startY, endX, (startY+endY)/2);
-            checkAndCut(paper, (startX+endX)/2 + 1, (startY+endY)/2 + 1, endX, endY);
+        if (isSameColor(square, startX, startY, endX, endY)) {
+            result[square[startX][startY]]++;
         } else {
-            if (criteria == 1) {
-                blue++;
-            } else {
-                white++;
+            int midX = startX + (endX - startX) / 2;
+            int midY = startY + (endY - startY) / 2;
+            // I
+            recursive(square, startX, startY, midX, midY, result);
+
+            // II
+            recursive(square, startX, midY, midX, endY, result);
+
+            // III
+            recursive(square, midX, startY, endX, midY, result);
+
+            // IV
+            recursive(square, midX, midY, endX, endY, result);
+        }
+    }
+
+    private static boolean isSameColor(int[][] square, int startX, int startY, int endX, int endY) {
+        int criteria = square[startX][startY];
+
+        for (int i = startX; i < endX; i++) {
+            for (int j = startY; j < endY; j++) {
+                if (square[i][j] != criteria) return false;
             }
         }
+
+        return true;
     }
 }

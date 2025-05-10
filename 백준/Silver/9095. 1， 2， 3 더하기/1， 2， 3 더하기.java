06f8677185c00a78b38d4894e899ad0dd4bc7ea7
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-05-10 14:36:32 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -4,32 +4,22 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] dp = new int[12];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= 11; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+
         int T = Integer.parseInt(br.readLine());
 
         while (T > 0) {
+            T--;
             int n = Integer.parseInt(br.readLine());
-
-            if (n == 1) {
-                bw.write("1");
-                bw.write("\n");
-                T--;
-                continue;
-            }
-
-            int[] dp = new int[n + 1];
-            dp[0] = 1;
-            dp[1] = 1;
-            dp[2] = 2;
-
-            for (int i = 3; i <= n; i++) {
-                dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
-            }
-
-
             bw.write(String.valueOf(dp[n]));
             bw.write("\n");
-
-            T--;
         }
 
         bw.flush();

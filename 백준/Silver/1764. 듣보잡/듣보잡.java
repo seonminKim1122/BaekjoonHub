d23aa0c2e0cf0a9b2f6eb69c72ce6b877b2bf66c
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-05-15 07:50:29 +0900

    [Silver IV] Title: 듣보잡, Time: 252 ms, Memory: 26076 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -11,21 +11,27 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        Set<String> set = new HashSet<>();
+        Set<String> set1 = new HashSet<>();
         for (int i = 0; i < N; i++) {
-            set.add(br.readLine());
+            String name = br.readLine(); // 듣도 못한 사람
+            set1.add(name);
         }
 
-        int answer = 0;
-        List<String> answerList = new ArrayList<>();
-        for (int i = 0; i < M; i++) {
-            String name = br.readLine();
-            if (set.contains(name)) {
-                answer++;
-                answerList.add(name);
+        List<String> names = new ArrayList<>();
+        for (int j = 0; j < M; j++) {
+            String name = br.readLine(); // 보도 못한 사람
+            if (set1.contains(name)) {
+                names.add(name);
             }
         }
+        names.sort(String::compareTo);
+
+        StringBuilder answer = new StringBuilder();
+        answer.append(names.size()).append("\n");
+        for (String name : names) {
+            answer.append(name).append("\n");
+        }
+
         System.out.println(answer);
-        answerList.stream().sorted().forEach(System.out::println);
     }
 }

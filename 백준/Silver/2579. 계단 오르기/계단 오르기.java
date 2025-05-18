d1de45df14724aff9c56475ca252ca1127120f4b
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-05-18 12:55:31 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,26 +1,35 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine()); // 계단의 갯수
-
-        int[] dp = new int[N+1];
-        int[] scores = new int[N+1];
-        for (int i = 1; i < N+1; i++) {
-            int score = Integer.parseInt(br.readLine());
-            scores[i] = score;
-            if (i <= 2) {
-                dp[i] = dp[i-1] + score;
-                continue;
-            }
-            // i-1 번째 score :
-            if (dp[i-3] + scores[i-1] > dp[i-2]) { // i-3 => i-1 => i vs i-2 => i
-                dp[i] = dp[i-3] + scores[i-1] + score;
-            } else {
-                dp[i] = dp[i-2] + score;
-            }
+        /*
+        N번째 계단에 도달하는 방법
+        : N - 2번째 계단에서 2칸 점프
+        : N - 3, N - 1, N 순서로 이동
+         */
+        int N = Integer.parseInt(br.readLine()); // 계단의 개수
+        int[] steps = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            steps[i] = Integer.parseInt(br.readLine());
         }
-        System.out.println(dp[N]);
+
+        int[] result = new int[N + 1];
+        result[1] = steps[1];
+
+        if (N == 1) {
+            System.out.println(result[N]);
+            return;
+        }
+
+        result[2] = steps[1] + steps[2];
+        for (int i = 3; i <= N; i++) {
+            result[i] = Math.max(result[i - 2], result[i - 3] + steps[i - 1]) + steps[i];
+        }
+
+        System.out.println(result[N]);
     }
 }

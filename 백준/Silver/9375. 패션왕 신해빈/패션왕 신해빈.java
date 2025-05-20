548fafa2aa434e1dcf804ae3ea783fa40dcc9be1
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-05-20 19:33:45 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 112 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -1,32 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < T; i++) {
-            int n = Integer.parseInt(br.readLine());
-            Map<String, Integer> map = new HashMap<>();
-            for (int j = 0; j < n; j++) {
-                String[] set = br.readLine().split(" ");
-                map.put(set[1], map.getOrDefault(set[1], 1)+1);
+        int T = Integer.parseInt(br.readLine()); // 테스트케이스
+        StringBuilder answer = new StringBuilder();
+        while (T > 0) {
+            T--;
+
+            int N = Integer.parseInt(br.readLine());
+            Map<String, Integer> clothMap = new HashMap<>(); // 카테고리별 옷의 수
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+
+                String cloth = st.nextToken();
+                String category = st.nextToken();
+
+                clothMap.put(category, clothMap.getOrDefault(category, 0) + 1);
             }
+
+            // 각 카테고리에서 선택할 수 있는 경우의 수 : clothMap.get(category) + 1
+            // 아무것도 안 입는 것은 불가능하므로 최종적으로 1 빼면 된다.
 
             int result = 1;
-            for (String key : map.keySet()) {
-                result *= map.get(key);
+            Set<String> categories = clothMap.keySet();
+            for (String category : categories) {
+                result *= (clothMap.get(category) + 1);
             }
-            result -= 1;
-            sb.append(result).append("\n");
+            result--;
+
+            answer.append(result).append("\n");
         }
 
-        System.out.println(sb);
+        System.out.println(answer);
     }
 }

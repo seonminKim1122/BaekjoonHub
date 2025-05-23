9dc7aa70174fb951b55eb9cf7eba2eec32e4186b
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-05-24 08:50:16 +0900

    [Silver III] Title: 2×n 타일링, Time: 132 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,21 +1,21 @@
-import java.util.Scanner;
-/*
-2 x n 직사각형 채우기
-1. 2 x (n-1) 직사각형에 2 x 1 타일 추가
-2. 2 x (n-2) 직사각형에 1 x 2 타일 2개 추가
- */
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-        long[] dp = new long[1001];
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[N + 1];
         dp[0] = 1;
         dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+        // N 만들기 : N - 1 에 2 x 1 타일 붙이기 or N - 2 에  1 x 2타일 2개 붙이기
+        for (int i = 2; i <= N; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
         }
 
-        System.out.println(dp[n]);
+        System.out.println(dp[N]);
     }
 }

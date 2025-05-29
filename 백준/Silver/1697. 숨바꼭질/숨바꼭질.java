dc934b358f63d5f422ac6de02dc1cdc377924b57
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-05-30 07:27:08 +0900

    [Silver I] Title: 숨바꼭질, Time: 148 ms, Memory: 18980 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,51 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int from = Integer.parseInt(st.nextToken());
-        int to = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
+        boolean[] visit = new boolean[100001];
         Queue<int[]> queue = new LinkedList<>();
-        boolean[] visited = new boolean[1000001];
+        queue.add(new int[]{N, 0});
+        visit[N] = true;
 
-        int result = 0;
-        queue.add(new int[]{from, 0});
-        visited[from] = true;
+        int answer = -1;
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
 
-        while (true) {
-            int[] temp = queue.poll();
-            int now = temp[0];
-            int sec = temp[1];
-            if (now == to) {
-                result = sec;
+            if (now[0] == K) {
+                answer = now[1];
                 break;
             }
 
-            if (now < 50001 && !visited[2 * now]) {
-                queue.add(new int[]{2 * now, sec + 1});
-                visited[2 * now] = true;
+            // + 1
+            if (now[0] + 1 <= 100000 && !visit[now[0] + 1]) {
+                queue.add(new int[]{now[0] + 1, now[1] + 1});
+                visit[now[0] + 1] = true;
             }
 
-            if (now >= 1 && !visited[now - 1]) {
-                queue.add(new int[]{now - 1, sec + 1});
-                visited[now - 1] = true;
+            // -1
+            if (now[0] - 1 >= 0 && !visit[now[0] - 1]) {
+                queue.add(new int[]{now[0] - 1, now[1] + 1});
+                visit[now[0] - 1] = true;
             }
 
-            if (now < 100000 && !visited[now + 1]) {
-                queue.add(new int[]{now + 1, sec + 1});
-                visited[now + 1] = true;
+            // * 2
+            if (2 * now[0] <= 100000 && !visit[2 * now[0]]) {
+                queue.add(new int[]{2 * now[0], now[1] + 1});
+                visit[2 * now[0]] = true;
             }
-
         }
 
-        System.out.println(result);
+        System.out.println(answer);
     }
+
+
 }

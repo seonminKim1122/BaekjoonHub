298ebf8a0db672cfdbee80910635790906d29864
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-06-08 13:38:30 +0900

    [Silver II] Title: 마인크래프트, Time: 736 ms, Memory: 35600 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.java
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.java
@@ -4,64 +4,53 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());   // 세로
+        int M = Integer.parseInt(st.nextToken());   // 가로
+        int B = Integer.parseInt(st.nextToken());   // 인벤토리 내 블록 수
 
-        int[][] ground = new int[N][M];
-        int min = 256;
-        int max = 0;
+        int[][] land = new int[N][M];
         for (int i = 0; i < N; i++) {
-            String[] temp = br.readLine().split(" ");
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                ground[i][j] = Integer.parseInt(temp[j]);
-                if (min > ground[i][j]) {
-                    min = ground[i][j];
-                }
-                if (max < ground[i][j]) {
-                    max = ground[i][j];
-                }
+                land[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        /*
-        min ~ max 의 높이로 만들기 위해 필요한 시간 중 최소값 출력,
-        만약 최소값이 여러 개라면 그 중 땅의 높이가 가장 높은 것 출력
-         */
-
-        int height = 0;
-        int resultTime = Integer.MAX_VALUE;
-
-        for (int i = min; i <= max; i++) {
+        // 가장 높은 높이부터 하나씩 낮춰가면서 맞추기(?)
+        // => 500 x 500 x 256
+        int minTime = Integer.MAX_VALUE;
+        int maxHeight = 0;
+        for (int H = 256; H >= 0; H--) {
+            int need = 0; // 모든 높이를 H로 만들기 위해 필요한 추가 블록 수
             int time = 0;
-            int block = B;
-            boolean isDone = true;
-            for (int j = 0; j < N; j++) {
-                for (int k = 0; k < M; k++) {
-                    if (ground[j][k] > i) {
-                        time += (ground[j][k] - i) * 2;
-                        block += (ground[j][k] - i);
-                    } else if (ground[j][k] < i) {
-                        time += (i - ground[j][k]);
-                        block -= (i - ground[j][k]);
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (land[i][j] < H) {
+                        need += (H - land[i][j]);
+                        time += (H - land[i][j]);
+                    } else if (land[i][j] > H) {
+                        need -= (land[i][j] - H);
+                        time += 2 * (land[i][j] - H);
                     }
                 }
             }
 
-            if (block < 0) {
-                isDone = false;
-            }
+            if (need > B) continue;
 
-            if (isDone && (time <= resultTime)) {
-                resultTime = time;
-                height = i;
+            if (minTime > time) {
+                minTime = time;
+                maxHeight = H;
+            } else if (minTime == time) {
+                maxHeight = Math.max(maxHeight, H);
             }
         }
 
-        System.out.println(resultTime + " " + height);
+        System.out.println(minTime + " " + maxHeight);
     }
 }

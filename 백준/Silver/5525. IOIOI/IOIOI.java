a8ced8b9ef70ac12cefceb60f3386be5e5fad768
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-06-14 14:37:37 +0900

    [Silver I] Title: IOIOI, Time: 244 ms, Memory: 22112 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -3,31 +3,50 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
-        String S = br.readLine();
 
-        StringBuilder temp = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            temp.append("IO");
-        }
-        temp.append("I");
+        char[] S = br.readLine().toCharArray();
 
-        String Pn = temp.toString();
+        int left = 0;
+        int right = 0;
 
-        int result = 0;
-        for (int i = 0; i < M; i++) {
-            if (S.charAt(i) == 'I' && i + 2 * N + 1 <= M) {
-                String sub = S.substring(i, i + 2 * N + 1);
-                if (sub.equals(Pn)) {
-                    result++;
+        int cnt = 1;
+        int answer = 0;
+        while (left < M && right < M) {
+            if (cnt == 2 * N + 1) {
+                answer++;
+                left += 2;
+                cnt -= 2;
+                continue;
+            }
+
+            // 1. left 가 I 가 아닌 경우 right + 1 로 시작점을 이동한다.
+            if (S[left] != 'I') {
+                right++;
+                left = right;
+                cnt = 1;
+            } else { // 2. left 가 I인 경우
+                // 2-1. right != right + 1 이면 right 만 이동
+                if (right < M - 1 && S[right] != S[right + 1]) {
+                    right++;
+                    cnt++;
+                } else {
+                // 2-2. right == right + 1 이면 right + 1 에서 재시작
+                    right++;
+                    left = right;
+                    cnt = 1;
                 }
             }
         }
 
-        System.out.println(result);
+        System.out.println(answer);
+
     }
+
 }

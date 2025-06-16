48c1b79bec818385eebc6524afa4fc9ee2a7ee1c
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-06-16 20:20:01 +0900

    [Gold V] Title: AC, Time: 636 ms, Memory: 87548 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -1,68 +1,75 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Deque;
-import java.util.LinkedList;
+import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
+        StringBuilder answer = new StringBuilder();
+        while (T > 0) {
+            T--;
 
-
-        for (int i = 0; i < T; i++) {
-            try {
-                String[] functions = br.readLine().split("");
-                int n = Integer.parseInt(br.readLine());
-                String[] temp = br.readLine().replace("[","").replace("]","").split(",");
-
-                Deque<Integer> deque = new LinkedList<>();
-                if (n != 0) {
-                    for (int j = 0; j < temp.length; j++) {
-                        int num = Integer.parseInt(temp[j]);
-                        deque.add(num);
-                    }
-                }
-
-
-                boolean isReversed = false;
-                for (String function : functions) {
-                    if (function.equals("D")) {
-                        if (isReversed) {
-                            deque.removeLast();
-                        } else {
-                            deque.removeFirst();
-                        }
-                    } else {
-                        isReversed = !isReversed;
-                    }
-                }
-
-                sb.append("[");
-                if (isReversed) {
-                    while (true) {
-                        if (deque.isEmpty()) break;
-                        sb.append(deque.pollLast());
-                        if (deque.isEmpty()) break;
-                        sb.append(",");
-                    }
-                } else {
-                    while (true) {
-                        if (deque.isEmpty()) break;
-                        sb.append(deque.pollFirst());
-                        if (deque.isEmpty()) break;
-                        sb.append(",");
-                    }
-                }
-                sb.append("]\n");
-
-            } catch (Exception e) {
-                sb.append("error\n");
+            char[] p = br.readLine().toCharArray();
+            int n = Integer.parseInt(br.readLine());
+            String input = br.readLine();
+            Deque<String> deque = new LinkedList<>();
+            if (n != 0) {
+                String[] numbers = input.substring(1, input.length() - 1).split(",");
+                Collections.addAll(deque, numbers);
             }
 
+
+
+            int direct = 0; // 0 이면 정방향, 1이면 역방향
+            boolean isError = false;
+            for (char order: p) {
+                if (order == 'R') {
+                    direct = (direct + 1) % 2;
+                } else {
+                    if (deque.isEmpty()) {
+                        isError = true;
+                        break;
+                    }
+
+                    if (direct == 0) {
+                        deque.removeFirst();
+                    } else {
+                        deque.removeLast();
+                    }
+                }
+            }
+
+            if (isError) {
+                answer.append("error").append("\n");
+            } else {
+                StringBuilder temp = new StringBuilder();
+                temp.append("[");
+                if (direct == 0) {
+                    while (!deque.isEmpty()) {
+                        temp.append(deque.removeFirst()).append(",");
+                    }
+                } else {
+                    while (!deque.isEmpty()) {
+                        temp.append(deque.removeLast()).append(",");
+                    }
+                }
+
+                if (temp.length() > 1) {
+                    temp.setLength(temp.length() - 1);
+                }
+
+                temp.append("]").append("\n");
+
+                answer.append(temp);
+            }
         }
-        System.out.println(sb);
+
+        System.out.println(answer);
     }
+
 }

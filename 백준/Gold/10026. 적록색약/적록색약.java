dffca71ce15ee2278f34896567357c29d95be4b8
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-06-17 21:35:14 +0900

    [Gold V] Title: 적록색약, Time: 140 ms, Memory: 17324 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -4,73 +4,71 @@ import java.io.InputStreamReader;
 
 public class Main {
 
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
     static int N;
-    static char[][] board;
-
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
-        board = new char[N][N];
+        char[][] map = new char[N][N];
         for (int i = 0; i < N; i++) {
-            String input = br.readLine();
-            for (int j = 0; j < N; j++) {
-                board[i][j] = input.charAt(j);
-            }
+            map[i] =  br.readLine().toCharArray();;
         }
 
-        boolean[][] visited1 = new boolean[N][N];
-        boolean[][] visited2 = new boolean[N][N];
 
-        int numOfDistrict1= 0;
-        int numOfDistrict2 = 0;
+        int normal = 0;
+        int week = 0;
+        boolean[][] normalVisit = new boolean[N][N];
+        boolean[][] weekVisit = new boolean[N][N];
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (!visited1[i][j]) {
-                    dfs1(i, j, visited1);
-                    numOfDistrict1++;
+                if (!normalVisit[i][j]) {
+                    normalDfs(map, i, j, normalVisit);
+                    normal++;
                 }
 
-                if (!visited2[i][j]) {
-                    dfs2(i, j, visited2);
-                    numOfDistrict2++;
+                if (!weekVisit[i][j]) {
+                    weekDfs(map, i, j, weekVisit);
+                    week++;
                 }
             }
         }
 
-        System.out.println(numOfDistrict1 + " " + numOfDistrict2);
+        System.out.println(normal + " " + week);
     }
 
-    public static void dfs1(int i, int j, boolean[][] visited) {
-        visited[i][j] = true;
+    private static void normalDfs(char[][] map, int y, int x, boolean[][] visit) {
+        visit[y][x] = true;
 
         for (int k = 0; k < 4; k++) {
-            int x = i + dx[k];
-            int y = j + dy[k];
+            int ny = y + dy[k];
+            int nx = x + dx[k];
 
-            if (x < 0 || y < 0 || x >= N || y >= N || visited[x][y] || board[i][j] != board[x][y]) continue;
+            if (ny < 0 || nx < 0 || ny >= N || nx >= N) continue;
+            if (visit[ny][nx]) continue;
+            if (map[ny][nx] != map[y][x]) continue;
 
-            dfs1(x, y, visited);
+            normalDfs(map, ny, nx, visit);
         }
     }
 
-    public static void dfs2(int i , int j, boolean[][] visited) {
-        visited[i][j] = true;
+    private static void weekDfs(char[][] map, int y, int x, boolean[][] visit) {
+        visit[y][x] = true;
 
         for (int k = 0; k < 4; k++) {
-            int x = i + dx[k];
-            int y = j + dy[k];
+            int ny = y + dy[k];
+            int nx = x + dx[k];
 
-            if (x < 0 || y < 0 || x >= N || y >= N || visited[x][y]) continue;
+            if (ny < 0 || nx < 0 || ny >= N || nx >= N) continue;
+            if (visit[ny][nx]) continue;
+            if (map[y][x] == 'B' && map[y][x] != map[ny][nx]) continue;
+            if ((map[y][x] == 'R' || map[y][x] == 'G') && map[ny][nx] == 'B') continue;
 
-            int diff = Math.abs(board[i][j] - board[x][y]);
-
-            if (diff == 11 || diff == 0) { // R : 82, G : 71, B : 66
-                dfs2(x, y, visited);
-            }
+            weekDfs(map, ny, nx, visit);
         }
     }
+
 }

38b6aeb562f7ca78e29efcf5ef8e229766d23f34
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-07-02 20:15:45 +0900

    [Silver II] Title: A → B, Time: 660 ms, Memory: 994888 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.java
+++ 백준/Silver/16953. A → B/A → B.java
@@ -1,32 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
 
-        int cnt = 1;
-        while (A != B) {
-            if (A > B || (B % 10 != 1 && B % 2 != 0)) {
-                cnt = -1;
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[] visit = new boolean[B + 1];
+        queue.add(new int[]{B, 0});
+        visit[B] = true;
+
+        int answer = -1;
+        while (!queue.isEmpty()) {
+            int[] temp = queue.poll();
+
+            int now = temp[0];
+            if (now == A) {
+                answer = temp[1] + 1;
                 break;
             }
 
-            if (B % 10 == 1) {
-                B = (B-1) / 10;
-            } else {
-                B /= 2;
+            int next = -1;
+            if (now % 10 == 1) {
+                next = now / 10;
+            } else if (now % 2 == 0) {
+                next = now / 2;
             }
 
-            cnt++;
+            if (next != -1 && !visit[next]) {
+                queue.add(new int[]{next, temp[1] + 1});
+                visit[next] = true;
+            }
         }
 
-        System.out.println(cnt);
+        System.out.println(answer);
     }
+
 }

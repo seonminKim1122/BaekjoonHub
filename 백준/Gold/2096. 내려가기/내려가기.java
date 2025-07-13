aa90623aa1fd4a1bf512d47e39f647508ddb9f5c
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-07-13 14:28:49 +0900

    [Gold V] Title: 내려가기, Time: 408 ms, Memory: 54044 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -1,41 +1,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
+
+    static final int INF = 100000 * 999;
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-
-        int[][] numbers = new int[N][3];
+        int[][] board = new int[N][3];
         for (int i = 0; i < N; i++) {
-            String[] input = br.readLine().split(" ");
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 3; j++) {
-                numbers[i][j] = Integer.parseInt(input[j]);
+                board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int[][] dp = new int[N][3];
+        System.out.print(max(board, N) + " " + min(board, N));
+    }
 
-        dp[0] = numbers[0];
+    static int max(int[][] board, int N) {
 
-        for (int i = 1; i < N; i++) {
-            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]) + numbers[i][0];
-            dp[i][1] = Math.max(Math.max(dp[i - 1][0], dp[i - 1][1]), dp[i - 1][2]) + numbers[i][1];
-            dp[i][2] = Math.max(dp[i - 1][1], dp[i - 1][2]) + numbers[i][2];
-        }
-
-        int max = Math.max(Math.max(dp[N - 1][0], dp[N - 1][1]), dp[N - 1][2]);
+        int[][] scores = new int[N][3];
+        scores[0] = board[0];
 
         for (int i = 1; i < N; i++) {
-            dp[i][0] = Math.min(dp[i - 1][0], dp[i - 1][1]) + numbers[i][0];
-            dp[i][1] = Math.min(Math.min(dp[i - 1][0], dp[i - 1][1]), dp[i - 1][2]) + numbers[i][1];
-            dp[i][2] = Math.min(dp[i - 1][1], dp[i - 1][2]) + numbers[i][2];
+            scores[i][0] = Math.max(scores[i - 1][0], scores[i - 1][1]) + board[i][0];
+            scores[i][1] = Math.max(Math.max(scores[i - 1][0], scores[i - 1][1]), scores[i - 1][2]) + board[i][1];
+            scores[i][2] = Math.max(scores[i - 1][1], scores[i - 1][2]) + board[i][2];
         }
 
-        int min = Math.min(Math.min(dp[N - 1][0], dp[N - 1][1]), dp[N - 1][2]);
+        return Math.max(Math.max(scores[N - 1][0], scores[N - 1][1]), scores[N - 1][2]);
+    }
 
-        System.out.println(max + " " + min);
+    static int min(int[][] board, int N) {
+
+        int[][] scores = new int[N][3];
+        scores[0] = board[0];
+
+        for (int i = 1; i < N; i++) {
+            scores[i][0] = Math.min(scores[i - 1][0], scores[i - 1][1]) + board[i][0];
+            scores[i][1] = Math.min(Math.min(scores[i - 1][0], scores[i - 1][1]), scores[i - 1][2]) + board[i][1];
+            scores[i][2] = Math.min(scores[i - 1][1], scores[i - 1][2]) + board[i][2];
+        }
+
+        return Math.min(Math.min(scores[N - 1][0], scores[N - 1][1]), scores[N - 1][2]);
     }
 }

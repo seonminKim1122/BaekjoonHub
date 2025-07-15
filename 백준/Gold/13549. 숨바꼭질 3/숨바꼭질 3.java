c549ed0f6e1dde7b75e485b3db776fda0abda83e
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-07-16 07:27:26 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 156 ms, Memory: 25528 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,11 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -13,38 +15,47 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
+        System.out.println(solve(N, K));
+    }
+
+    static int solve(int N, int K) {
+
+        int[] times = new int[100001];
+        Arrays.fill(times, 100001);
 
         Queue<int[]> queue = new LinkedList<>();
-        boolean[] visited = new boolean[100001];
         queue.add(new int[]{N, 0});
-        visited[N] = true;
+        times[N] = 0;
 
         while (!queue.isEmpty()) {
-            int[] curr = queue.poll();
-            int X = curr[0];
-            int time = curr[1];
+            int[] now = queue.poll();
 
-            if (X == K) {
-                System.out.println(time);
-                return;
+            // X - 1
+            int next = now[0] - 1;
+            if (isValid(next) && times[next] > now[1] + 1) {
+                queue.add(new int[]{next, now[1] + 1});
+                times[next] = now[1] + 1;
             }
 
-            int temp = X;
-            while (2 * temp <= 100000 && !visited[2 * temp]) {
-                queue.add(new int[]{2 * temp, time});
-                visited[2 * temp] = true;
-                temp = 2 * temp;
+            // X + 1
+            next = now[0] + 1;
+            if (isValid(next) && times[next] > now[1] + 1) {
+                queue.add(new int[]{next, now[1] + 1});
+                times[next] = now[1] + 1;
             }
 
-            if (X - 1 >= 0 && !visited[X - 1]) {
-                queue.add(new int[]{X - 1, time + 1});
-                visited[X - 1] = true;
-            }
-
-            if (X + 1 <= 100000 && !visited[X + 1]) {
-                queue.add(new int[]{X + 1, time + 1});
-                visited[X + 1] = true;
+            // 2 * X
+            next = 2 * now[0];
+            if (isValid(next) && times[next] > now[1]) {
+                queue.add(new int[]{next, now[1]});
+                times[next] = now[1];
             }
         }
+
+        return times[K];
+    }
+
+    static boolean isValid(int index) {
+        return index >= 0 && index <= 100000;
     }
 }

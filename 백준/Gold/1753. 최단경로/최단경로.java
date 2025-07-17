442d8e6a6276a47baa2610901451bd4a223917d3
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-07-17 19:26:16 +0900

    [Gold IV] Title: 최단경로, Time: 1044 ms, Memory: 131928 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -5,70 +5,77 @@ import java.util.*;
 
 public class Main {
 
-    static final int INF = 987654321;
+    static final int INF = 20000 * 10;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int V = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(br.readLine()) - 1;
+        int V = Integer.parseInt(st.nextToken()); // 정점의 개수
+        int E = Integer.parseInt(st.nextToken()); // 간선의 개수
+        int K = Integer.parseInt(br.readLine());  // 시작 정점
 
-        List<List<Node>> graph = new ArrayList<>();
-        for (int v = 0; v < V; v++) {
-            graph.add(new ArrayList<>());
+        List<Map<Integer, Integer>> graph = new ArrayList<>();
+        for (int i = 0; i <= V; i++) {
+            graph.add(new HashMap<>());
         }
 
-        for (int e = 0; e < E; e++) {
+        for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
 
-            int u = Integer.parseInt(st.nextToken()) - 1;
-            int v = Integer.parseInt(st.nextToken()) - 1;
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
 
-            graph.get(u).add(new Node(v, w));
-        }
-
-        int[] result = dijkstra(graph, K);
-        for (int i = 0; i < result.length; i++) {
-            if (result[i] >= INF) {
-                System.out.println("INF");
+            Map<Integer, Integer> map = graph.get(u);
+            if (map.containsKey(v)) {
+                map.put(v, Math.min(map.get(v), w));
             } else {
-                System.out.println(result[i]);
+                map.put(v, w);
             }
         }
+
+        System.out.println(solve(graph, K, V));
     }
 
-    static int[] dijkstra(List<List<Node>> graph, int K) {
-        int[] dist = new int[graph.size()];
-        PriorityQueue<Node> pq = new PriorityQueue<>((n1, n2) -> dist[n1.vertex] - dist[n2.vertex]);
 
+    static String solve(List<Map<Integer, Integer>> graph, int K, int V) {
+
+        int[] dist = new int[V + 1];
         Arrays.fill(dist, INF);
         dist[K] = 0;
-        pq.add(new Node(K, 0)); // 시작 정점
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((i1, i2) -> i1[1] - i2[1]);
+        boolean[] used = new boolean[V + 1];
+        pq.add(new int[]{K, 0});
+        used[K] = true;
 
         while (!pq.isEmpty()) {
-            Node cur = pq.poll();
+            int[] now = pq.poll();
 
-            for (Node node : graph.get(cur.vertex)) {
-                if (dist[node.vertex] > dist[cur.vertex] + node.weight) {
-                    dist[node.vertex] = dist[cur.vertex] + node.weight;
-                    pq.add(node);
+            used[now[0]] = true;
+
+            Map<Integer, Integer> edgeMap = graph.get(now[0]);
+            for (int to : edgeMap.keySet()) {
+                if (used[to]) continue;
+
+                if (dist[to] > dist[now[0]] + edgeMap.get(to)) {
+                    dist[to] = dist[now[0]] + edgeMap.get(to);
+                    pq.add(new int[]{to, dist[to]});
                 }
             }
         }
 
-        return dist;
-    }
-
-    static class Node {
-        int vertex;
-        int weight;
-
-        Node(int vertex, int weight) {
-            this.vertex = vertex;
-            this.weight = weight;
+        StringBuilder answer = new StringBuilder();
+        for (int i = 1; i <= V; i++) {
+            if (dist[i] == INF) {
+                answer.append("INF");
+            } else {
+                answer.append(dist[i]);
+            }
+            answer.append("\n");
         }
+
+        return answer.toString();
     }
 }

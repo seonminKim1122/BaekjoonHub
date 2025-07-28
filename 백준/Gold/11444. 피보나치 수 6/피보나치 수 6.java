9e8070713086ed9fdc1e19eeaab54bf359215dd8
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-07-28 20:27:32 +0900

    [Gold II] Title: 피보나치 수 6, Time: 100 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.java
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.java
@@ -4,38 +4,41 @@ import java.io.InputStreamReader;
 
 public class Main {
 
+    static final int DIV = 1_000_000_007;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         long n = Long.parseLong(br.readLine());
-        long[][] arr = {{1, 1}, {1, 0}};
 
-        System.out.println(fibonacci(arr, n - 1)[0][0]);
+        int[][] matrix = new int[][]{{1, 1}, {1, 0}};
+        int[][] fiboMatrix = power(matrix, n);
+
+        System.out.println(fiboMatrix[1][0]);
     }
 
-    public static long[][] fibonacci(long[][] arr, long exp) {
-        if (exp == 1 || exp == 0) return arr;
-
-        long[][] result = fibonacci(arr, exp / 2);
-
-        result = multiply(result, result);
-
-        if (exp % 2 == 1) {
-            result = multiply(result, arr);
+    public static int[][] power(int[][] matrix, long n) {
+        if (n == 0) {
+            return new int[][]{{1, 0}, {0, 1}};
+        } else if (n == 1) {
+            return matrix;
         }
 
-        return result;
+        int[][] temp = power(matrix, n / 2);
+        if (n % 2 == 0) {
+            return multiply(temp, temp);
+        } else {
+            return multiply(multiply(temp, temp), matrix);
+        }
     }
 
-    public static long[][] multiply(long[][] arr1, long[][] arr2) {
-        long[][] result = new long[2][2];
+    public static int[][] multiply(int[][] matrix1, int[][] matrix2) {
+        
+        int num1 = (int)((((long)matrix1[0][0] * (long)matrix2[0][0]) % DIV + ((long)matrix1[0][1] * (long)matrix2[1][0]) % DIV) % DIV);
+        int num2 = (int)((((long)matrix1[0][0] * (long)matrix2[0][1]) % DIV + ((long)matrix1[0][1] * (long)matrix2[1][1]) % DIV) % DIV);
+        int num3 = (int)((((long)matrix1[1][0] * (long)matrix2[0][0]) % DIV + ((long)matrix1[1][1] * (long)matrix2[1][0]) % DIV) % DIV);
+        int num4 = (int)((((long)matrix1[1][0] * (long)matrix2[0][1]) % DIV + ((long)matrix1[1][1] * (long)matrix2[1][1]) % DIV) % DIV);
 
-        result[0][0] = (arr1[0][0] * arr2[0][0] + arr1[0][1] * arr2[1][0]) % 1_000_000_007;
-        result[0][1] = (arr1[0][0] * arr2[0][1] + arr1[0][1] * arr2[1][1]) % 1_000_000_007;
-        result[1][0] = (arr1[1][0] * arr2[0][0] + arr1[1][1] * arr2[1][0]) % 1_000_000_007;
-        result[1][1] = (arr1[1][0] * arr2[0][1] + arr1[1][1] * arr2[1][1]) % 1_000_000_007;
-
-        return result;
+        return new int[][]{{num1, num2}, {num3, num4}};
     }
+
 }

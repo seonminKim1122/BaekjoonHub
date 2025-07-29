b0f036c4d2796f13c565660142c2c3e5b6be72c5
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-07-29 21:44:18 +0900

    [Gold V] Title: 치킨 배달, Time: 200 ms, Memory: 17484 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,71 +1,98 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N;
-    static int M;
-    static List<int[]> houses;
-    static List<int[]> chickenRestaurants;
-    static boolean[] isOpen;
-    static int result = Integer.MAX_VALUE;
+    static int answer = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        houses = new ArrayList<>();
-        chickenRestaurants = new ArrayList<>();
-
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] city = new int[N][N];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                int input = Integer.parseInt(st.nextToken());
-                if (input == 1) {
-                    houses.add(new int[]{i, j});
-                } else if (input == 2) {
-                    chickenRestaurants.add(new int[]{i, j});
+                city[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        List<House> houses = new ArrayList<>();
+        List<ChickenStore> chickenStores = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (city[i][j] == 2) {
+                    chickenStores.add(new ChickenStore(i, j));
+                } else if (city[i][j] == 1) {
+                    houses.add(new House(i, j));
                 }
             }
         }
 
-        isOpen = new boolean[chickenRestaurants.size()];
-        dfs(0, 0);
-
-        System.out.println(result);
+        int totalSize = chickenStores.size();
+        solve(houses, chickenStores, 0, totalSize - M, totalSize, 0);
+        System.out.println(answer);
     }
 
-    public static void dfs(int start, int cnt) {
-        if (cnt == M) {
-            int temp = 0;
-            for (int[] house : houses) {
-                int min = 2 * N;
-                for (int i = 0; i < chickenRestaurants.size(); i++) {
-                    if (!isOpen[i]) continue;
-
-                    int[] chickenRestaurant = chickenRestaurants.get(i);
-                    int distance = Math.abs(house[0] - chickenRestaurant[0]) + Math.abs(house[1] - chickenRestaurant[1]);
-
-                    if (distance < min) {
-                        min = distance;
-                    }
-                }
-                temp += min;
-            }
-
-            result = Math.min(result, temp);
+    static void solve(List<House> houses, List<ChickenStore> chickenStores, int depth, int target, int totalSize, int start) {
+        if (calcChickenDistance(houses, chickenStores) > answer) {
             return;
         }
 
-        for (int i = start; i < chickenRestaurants.size(); i++) {
-            isOpen[i] = true;
-            dfs(i + 1, cnt + 1);
-            isOpen[i] = false;
+        if (depth == target) {
+            answer = Math.min(answer, calcChickenDistance(houses, chickenStores));
+            return;
+        }
+
+
+        for (int i = start; i < totalSize; i++) {
+            ChickenStore chickenStore = chickenStores.get(i);
+            if (chickenStore.isClose) continue;
+            chickenStore.isClose = true;
+            solve(houses, chickenStores, depth + 1, target, totalSize, i + 1);
+            chickenStore.isClose = false;
+        }
+
+    }
+
+    static int calcChickenDistance(List<House> houses, List<ChickenStore> chickenStores) {
+        int result = 0;
+        for (House house: houses) {
+            int distance = Integer.MAX_VALUE;
+            for (ChickenStore chickenStore: chickenStores) {
+                if (chickenStore.isClose) continue;
+                distance = Math.min(distance, Math.abs(house.y - chickenStore.y) + Math.abs(house.x - chickenStore.x));
+            }
+            result += distance;
+        }
+        return result;
+    }
+
+    static class House {
+        int y;
+        int x;
+
+         House(int y, int x) {
+             this.y = y;
+             this.x = x;
+         }
+    }
+
+    static class ChickenStore {
+        int y;
+        int x;
+        boolean isClose;
+
+        ChickenStore(int y, int x) {
+            this.y = y;
+            this.x = x;
+            isClose = false;
         }
     }
 }

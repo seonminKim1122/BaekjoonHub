905d9d4154cbabb40c5f272c35b39b422bd5a8c0
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-08-01 21:17:16 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 468 ms, Memory: 145188 KB -BaekjoonHub

--- 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
@@ -4,43 +4,45 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    static char[][] board;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
-        board = new char[N][2 * N - 1];
 
+        char[][] board = new char[N][N * 2 - 1];
+        drawTriangle(N, 0, N - 1, board);
+
+        StringBuilder answer = new StringBuilder();
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < 2 * N - 1; j++) {
-                board[i][j] = ' ';
+                if (board[i][j] != '*') {
+                    answer.append(" ");
+                } else {
+                    answer.append("*");
+                }
             }
+            answer.append("\n");
         }
 
-        draw(0, 0, N);
-
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < 2 * N - 1; j++) {
-                result.append(board[i][j]);
-            }
-            result.append("\n");
-        }
-
-        System.out.println(result);
+        System.out.println(answer);
     }
 
-    public static void draw(int i, int j, int N) {
+    static void drawTriangle(int N, int i, int j, char[][] board) { // i, j 를 꼭대기로 하는 N줄 삼각형 별 찍기
         if (N == 3) {
-            board[i][j + 2] = '*';
-            board[i + 1][j + 1] = board[i + 1][j + 3] = '*';
-            board[i + 2][j] = board[i + 2][j + 1] = board[i + 2][j + 2] = board[i + 2][j + 3] = board[i + 2][j + 4] = '*';
+            board[i][j] = '*';
+
+            board[i + 1][j - 1] = '*';
+            board[i + 1][j + 1] = '*';
+
+            board[i + 2][j - 2] = '*';
+            board[i + 2][j - 1] = '*';
+            board[i + 2][j] = '*';
+            board[i + 2][j + 1] = '*';
+            board[i + 2][j + 2] = '*';
             return;
         }
 
-        draw(i, j + N / 2, N / 2);
-        draw(i + N / 2, j, N / 2);
-        draw(i + N / 2, j + N, N / 2);
+        drawTriangle(N / 2, i, j, board);
+        drawTriangle(N / 2, i + N / 2, j - N / 2, board);
+        drawTriangle(N / 2, i + N / 2, j + N / 2, board);
     }
 }

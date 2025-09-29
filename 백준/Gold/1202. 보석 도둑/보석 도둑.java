08a105a3f4569a05ea7172fe525f99d23405c9f8
seonminKim1122 <124031561+seonminKim1122@users.noreply.github.com>
2025-09-29 19:08:55 +0900

    [Gold II] Title: 보석 도둑, Time: 1660 ms, Memory: 124080 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -1,7 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -9,69 +12,55 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());   // 보석의 갯수
+        int K = Integer.parseInt(st.nextToken());   // 가방의 갯수
 
-        Jewelry[] jewerlies = new Jewelry[N];
+        Jewelry[] jewelries = new Jewelry[N];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int M = Integer.parseInt(st.nextToken());
             int V = Integer.parseInt(st.nextToken());
 
-            Jewelry jewelry = new Jewelry(M, V);
-            jewerlies[i] = jewelry;
+            jewelries[i] = new Jewelry(M, V);
         }
-        Arrays.sort(jewerlies, new Comparator<Jewelry>() {
-            @Override
-            public int compare(Jewelry o1, Jewelry o2) {
-                return o1.weight - o2.weight;
-            }
-        });
 
-        int[] bags = new int[K];
+        int[] C = new int[K];
         for (int i = 0; i < K; i++) {
-            int bagSize = Integer.parseInt(br.readLine());
-            bags[i] = bagSize;
+            C[i] = Integer.parseInt(br.readLine());
         }
-        Arrays.sort(bags);
 
-        PriorityQueue<Jewelry> pq = new PriorityQueue<>();
-        int bagIdx = 0;
-        int jewelryIdx = 0;
+        Arrays.sort(jewelries);
+        Arrays.sort(C);
+
         long result = 0;
-        while (bagIdx < K && jewelryIdx < N) {
-            Jewelry jewelry = jewerlies[jewelryIdx];
-            if (jewelry.weight <= bags[bagIdx]) {
-                pq.add(jewelry);
-                jewelryIdx++;
-            } else {
-                if (!pq.isEmpty()) {
-                    result += pq.poll().value;
-                }
-                bagIdx++;
+        int idx = 0;
+        PriorityQueue<Integer> pq  = new PriorityQueue<>(Collections.reverseOrder());
+        for (int c : C) {
+            while (idx < N && jewelries[idx].M <= c) {
+                pq.add(jewelries[idx].V);
+                idx++;
             }
-        }
 
-        while (bagIdx < K && !pq.isEmpty()) {
-            result += pq.poll().value;
-            bagIdx++;
+            if (!pq.isEmpty()) {
+                result += pq.poll();
+            }
         }
 
         System.out.println(result);
     }
 
     static class Jewelry implements Comparable<Jewelry> {
-        int weight;
-        int value;
+        int M;
+        int V;
 
-        Jewelry(int weight, int value) {
-            this.weight = weight;
-            this.value = value;
+        Jewelry(int M, int V) {
+            this.M = M;
+            this.V = V;
         }
 
         @Override
-        public int compareTo(Jewelry o) {
-            return o.value - this.value;
+        public int compareTo(Jewelry other) {
+            return this.M - other.M;
         }
     }
 }
